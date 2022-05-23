b06ff66ee160bc89113ac5e02e36957adcd4d258
doogie97 <chris_97@naver.com>
2022-05-23 18:32:13 +0900

    📱storyboard : 기본 Layout 구성

---
+++ LOARANG/LOARANG/BookMarkMainCell.swift
@@ -0,0 +1,17 @@
+//
+//  BookMarkMainCell.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/05/23.
+//
+
+import UIKit
+
+class BookMarkMainCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
