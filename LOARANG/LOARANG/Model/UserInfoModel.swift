16a63d29db1bd13d0cc106615bb563651f99b22c
doogie97 <chris_97@naver.com>
2022-05-26 01:25:42 +0900

    💎feat : UserInfo Model 생성

---
+++ LOARANG/LOARANG/Model/UserInfoModel.swift
@@ -0,0 +1,17 @@
+//
+//  UserInfoModel.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/05/26.
+//
+
+struct UserInfo {
+    let name: String
+    let expeditionLevel: String
+    let battleLevel: String
+    let itemLevel: String
+    let title: String
+    let guild: String
+    let pvpGrade: String
+    let wisdom: String
+}
