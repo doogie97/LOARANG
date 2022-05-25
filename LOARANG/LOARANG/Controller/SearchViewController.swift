ebfbeec0324c4c11e534dcacdf77171b959b2ac2
doogie97 <chris_97@naver.com>
2022-05-26 01:34:35 +0900

    💎feat : UserInfoVC 생성 및 searchBar의 검색 버튼 입력시 이동 구현

--- LOARANG/LOARANG/Controller/SearchViewController.swift
+++ LOARANG/LOARANG/Controller/SearchViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    @IBOutlet weak var userSearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        userSearchBar.delegate = self
     }
     @IBAction func touchBackButton(_ sender: UIButton) {
         navigationController?.popViewController(animated: true)
     }
-    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.view.endEditing(true)
+        guard let userInfoVC = storyboard?.instantiateViewController(withIdentifier: "UserInfoViewController") else { return }
+        navigationController?.pushViewController(userInfoVC, animated: true)
+    }
 }
