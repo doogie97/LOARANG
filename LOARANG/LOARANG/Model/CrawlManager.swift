d674f4dc21b477ca4229c5022532338f31d10d62
doogie97 <chris_97@naver.com>
2022-05-26 01:26:38 +0900

    💎feat : 얻어오는 유저 정보 변경

--- LOARANG/LOARANG/Model/CrawlManager.swift
+++ LOARANG/LOARANG/Model/CrawlManager.swift
@@ -9,21 +9,26 @@ import Foundation
 import SwiftSoup
 
 struct CrawlManager {
-    func getCharacterTitle(userName: String) -> String {
+    func getUserInfo(userName: String) -> UserInfo? {
         
         let urlString = "https://lostark.game.onstove.com/Profile/Character/" + userName.changeToPercent()
-        guard let url = URL(string: urlString) else { return "실패"}
+        guard let url = URL(string: urlString) else { return nil}
         
         do {
             let html = try String(contentsOf: url, encoding: .utf8)
             let doc: Document = try SwiftSoup.parse(html)
             
-            let characterTitle: Elements = try doc.select(".game-info__title").select("span")
-            let titleText = try characterTitle.text()
-            return titleText
-        } catch {
-            print("error")
-        }
-        return "실패"
+            let name = try doc.select(".profile-character-info__name").select("span").text()
+            let expeditionLevel = try doc.select(".level-info__expedition").select("span").text()
+            let battleLevel = try doc.select(".level-info__item").select("span").text()
+            let itemLevel = try doc.select(".level-info2__expedition").select("span").text()
+            let guild = try doc.select(".game-info__guild").select("span").text()
+            let pvpLevel = try doc.select(".level-info__pvp").select("span").text()
+            let wisdom = try doc.select(".game-info__wisdom").select("span").text()
+            let title = try doc.select(".game-info__title").select("span").text()
+            
+            return UserInfo(name:name, expeditionLevel: expeditionLevel, battleLevel: battleLevel, itemLevel: itemLevel, title: title, guild: guild, pvpGrade: pvpLevel, wisdom: wisdom)
+        } catch {}
+        return nil
     }
 }
