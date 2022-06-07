9a584b1a258012c46d62d914bd720d4db5f69be6
doogie97 <chris_97@naver.com>
2022-06-07 10:39:29 +0900

    💎feat : accesseryable 프로토콜 생성

---
+++ LOARANG/LOARANG/Model/Protocol/Accessoryable.swift
@@ -0,0 +1,17 @@
+//
+//  Accessoryable.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/05/31.
+//
+
+protocol Accessoryable {
+    var name: String { get }
+    var type: String { get }
+    var tier: String { get }
+    var quality: String { get }
+    var str: String { get }
+    var dex: String { get }
+    var int: String { get }
+    var hp: String { get }
+}
