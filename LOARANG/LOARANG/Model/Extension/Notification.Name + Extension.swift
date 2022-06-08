52c87bf7db06e4ca3b0cf7a95f76e01ee89ce063
doogie97 <chris_97@naver.com>
2022-06-09 03:06:21 +0900

    💎feat : Notification.Name 확장 후 bookmark name 생성

---
+++ LOARANG/LOARANG/Model/Extension/Notification.Name + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name + Extension.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/06/09.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let bookmark = Notification.Name("bookmark")
+}
