fc06a438898b9443261e7bbfd7fa121db111f7d9
doogie97 <chris_97@naver.com>
2022-06-09 14:55:13 +0900

    📱storyboard : SettingViewController생성 및 레이아웃 설정

---
+++ LOARANG/LOARANG/View/Setting/SettingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SettingViewController.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/06/09.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    @IBOutlet private weak var titleLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
