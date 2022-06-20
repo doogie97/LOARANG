a8b04e472a4c58b80ef879276cdbdc00de66ffab
doogie97 <chris_97@naver.com>
2022-06-21 01:02:03 +0900

    💎feat : BasicAbility 콜렉션 뷰 cell 생성

---
+++ LOARANG/LOARANG/View/Home/UserInfo/BasicAbilityCVCell.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAbilityCVCell.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/06/21.
+//
+
+import UIKit
+
+class BasicAbilityCVCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
