5398593ab1061a3d8a9e009d2dc2595e99fd834a
doogie97 <chris_97@naver.com>
2022-06-20 17:37:04 +0900

    ♻️refactor : getCardInfo 리팩토링

--- LOARANG/LOARANG/Model/InfoDecoder.swift
+++ LOARANG/LOARANG/Model/InfoDecoder.swift
@@ -17,16 +17,12 @@ struct InfoDecoder {
         return getCardInfo(json: cardJson)
     }
     
-    private func getCardInfo(json: JSON) -> CardInfo {
-        let cardArray = json.sorted { first, second in
-            return first.0 < second.0
-        }
-        var carddd:[Card] = []
-        for i in cardArray {
-            let card = Card(name: i.1["Element_000"]["value"].stringValue,
-                            awakeCount: i.1["Element_001"]["value"]["awakeCount"].intValue,
-                            awakeTotal: i.1["Element_001"]["value"]["awakeTotal"].intValue)
-            carddd.append(card)
+    private func getCardInfo(json: JSON) -> [Card] {
+        let cardArray = json.sortedUp
+        let cards = cardArray.compactMap {
+            Card(name: $0["Element_000"]["value"].stringValue,
+                 awakeCount: $0["Element_001"]["value"]["awakeCount"].intValue,
+                 awakeTotal: $0["Element_001"]["value"]["awakeTotal"].intValue)
         }
         
         let userCards = CardInfo(first: carddd.popFirst() , second: carddd.popFirst(), third: carddd.popFirst(), fourth: carddd.popFirst(), fifth: carddd.popFirst(), sixth: carddd.popFirst())
