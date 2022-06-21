270610dc3aa2c16f2e905498c7c1a42d547dcf17
doogie97 <chris_97@naver.com>
2022-06-22 01:55:56 +0900

    💎feat : 캐릭터 장착 카드 수에 따른 카드 표시 기능 구현

--- LOARANG/LOARANG/View/Home/UserInfo/CardSetCVCell.swift
+++ LOARANG/LOARANG/View/Home/UserInfo/CardSetCVCell.swift
@@ -7,7 +7,74 @@
 
 import UIKit
 
-class CardSetCVCell: UICollectionViewCell {
-    @IBOutlet weak var cardName: UILabel!
+//MARK: - properties
+final class CardSetCVCell: UICollectionViewCell {
+    @IBOutlet private weak var firstCardNameLabel: UILabel!
+    @IBOutlet private weak var secondCardNameLabel: UILabel!
+    @IBOutlet private weak var thirdCardNameLabel: UILabel!
+    @IBOutlet private weak var fourthCardNameLabel: UILabel!
+    @IBOutlet private weak var fifthCardNameLabel: UILabel!
+    @IBOutlet private weak var sixthCardNameLabel: UILabel!
+    private lazy var cardLabels: [UILabel] = [firstCardNameLabel, secondCardNameLabel, thirdCardNameLabel, fourthCardNameLabel, fifthCardNameLabel, sixthCardNameLabel]
+    
+    @IBOutlet private weak var firstCardView: UIView!
+    @IBOutlet private weak var secondCardView: UIView!
+    @IBOutlet private weak var thirdCardView: UIView!
+    @IBOutlet private weak var fourthCardView: UIView!
+    @IBOutlet private weak var fifthCardView: UIView!
+    @IBOutlet private weak var sixthCardView: UIView!
+    private lazy var cardViews: [UIView] = [sixthCardView, fifthCardView, fourthCardView, thirdCardView, secondCardView, firstCardView]
+    
+    @IBOutlet weak var firstStackView: UIStackView!
+    @IBOutlet weak var secondStackView: UIStackView!
+    private var cards: [Card]?
+    private var effects: [CardSetEffect]?
+}
+//MARK: - method
+extension CardSetCVCell {
     func setCards(cardInfo: CardInfo) {
+        cards = cardInfo.cards
+        effects = cardInfo.effects
+        configureCell()
+    }
+    
+    private func configureCell() {
+        guard let cards = cards else {
+            return
+        }
+        if cards.count != 6 {
+            hideViews(cardViews.count - cards.count)
+        }
+        
+        for (index, card) in cards.enumerated() {
+            cardLabels[index].text = card.name
+        }
+    }
+    
+    private func hideViews(_ count: Int) {
+        if count >= 3 {
+            secondStackView.isHidden = true
+        }
+        if count == 6 {
+            firstStackView.addArrangedSubview(
+                noCardLabel())
+        }
+        for index in 0...count - 1 {
+            cardViews[index].isHidden = true
+        }
+        cardViews.reverse()
+        for _ in 0...count - 1 {
+            let _ = cardViews.popLast()
+        }
+    }
+    
+    private func noCardLabel() -> UILabel {
+        let label = UILabel()
+        label.text = "장착된 카드가 없습니다"
+        label.font = UIFont.one(size: 20, family: .Bold)
+        label.textColor = .white
+        label.textAlignment = .center
+        return label
+    }
+    
 }
