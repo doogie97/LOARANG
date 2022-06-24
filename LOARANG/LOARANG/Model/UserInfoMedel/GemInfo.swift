2275a58bc641c55bd088ebebaae3c5c0940a2dd0
doogie97 <chris_97@naver.com>
2022-06-24 18:06:02 +0900

    💎feat : GemInfo 모델 생성

---
+++ LOARANG/LOARANG/Model/UserInfoMedel/GemInfo.swift
@@ -0,0 +1,13 @@
+//
+//  GemInfo.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/06/24.
+//
+
+struct Gem {
+    let name: String
+    let grade: Int
+    let lvString: String
+    let effect: String
+}
