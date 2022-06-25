a36ced035853228bd7ea6de50ef3c65c6ac931fc
doogie97 <chris_97@naver.com>
2022-06-25 14:15:18 +0900

    💎feat : json에서 파츠 구별할 index enum으로 구현

--- LOARANG/LOARANG/Model/UserInfoMedel/Equipment.swift
+++ LOARANG/LOARANG/Model/UserInfoMedel/Equipment.swift
@@ -33,3 +33,35 @@ struct EquipmentPart {
     let thirdSetEffect: String
     let imageURL: String
 }
+
+enum EquimentIndex: String {
+    //장비
+    case weapon = "000"
+    case head = "001"
+    case top = "002"
+    case bottom = "003"
+    case glove = "004"
+    case shoulder = "005"
+    //악세
+    case necklace = "006"
+    case firstEarring = "007"
+    case secondEarring = "008"
+    case firstRing = "009"
+    case secondRing = "010"
+    //어빌리티스톤
+    case abilityStone = "011"
+    //팔찌
+    case bracelet = "026"
+    //아바타
+    case mainWeaponAvatar = "012"
+    case mainHeadAvatar = "013"
+    case mainTopAvatar = "014"
+    case mainBottomAvatar = "015"
+    case instrumentAvarat = "016"
+    case fisrtFaceAvarat = "017"
+    case secondFaceAvarat = "018"
+    case subWeaponAvatar = "019"
+    case subHeadAvatar = "020"
+    case subTopAvatar = "021"
+    case subBottomAvatar = "022"
+}
