94b7ea6d85280d1c1c4a267e36948a81253828e4
doogie97 <chris_97@naver.com>
2022-06-25 14:16:37 +0900

    💎feat : int를 세자리의 string으로 바꾸는 기능 구현

---
+++ LOARANG/LOARANG/Model/Extension/Int + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  Int + Extension.swift
+//  LOARANG
+//
+//  Created by 최최성균 on 2022/06/25.
+//
+
+extension Int {
+    var threeNum: String {
+        return String(format: "%03d", self)
+    }
+}
