f9562ebb13e70b3febcb12bcb008eaeff925a67b
doogie97 <chris_97@naver.com>
2022-06-25 14:17:57 +0900

    💎feat : EquipmentPart를 반환하는 연산 프로퍼티 생성

--- LOARANG/LOARANG/Model/Extension/JSON + Extension.swift
+++ LOARANG/LOARANG/Model/Extension/JSON + Extension.swift
@@ -39,5 +39,28 @@ extension JSON {
         return jsonArray
     }
     
+    var equipmentPart: EquipmentPart {
+        let isFullLv = self["Element_007"]["type"].stringValue == "Progress"
+        let elementNum = isFullLv ? 8 : 7
+        
+        let equipment = EquipmentPart(titlt: self["Element_000"]["value"].stringValue.centerName,
+                                  part: self["Element_001"]["value"]["leftStr0"].stringValue,
+                                  lv: self["Element_001"]["value"]["leftStr2"].stringValue.centerName,
+                                  quality: self["Element_001"]["value"]["qualityValue"].intValue,
+                                  grade: self["Element_001"]["value"]["slotData"]["iconGrade"].intValue,
+                                  basicEffects: self["Element_005"]["value"]["Element_001"].stringValue.replacingOccurrences(of: "<BR>", with: "\n"),
+                                  additionalEffects: self["Element_006"]["value"]["Element_001"].stringValue,
+                                  firstTripod: self["Element_\(elementNum.threeNum)"]["value"]["Element_000"]["contentStr"]["Element_000"]["contentStr"].stringValue,
+                                  secondTripod: self["Element_\(elementNum.threeNum)"]["value"]["Element_000"]["contentStr"]["Element_001"]["contentStr"].stringValue,
+                                  thirdTripod: self["Element_\(elementNum.threeNum)"]["value"]["Element_000"]["contentStr"]["Element_002"]["contentStr"].stringValue,
+                                  setEffectLv: self["Element_\((elementNum + 1).threeNum)"]["value"]["Element_001"].stringValue,
+                                  firstSetEffect: self["Element_\((elementNum + 2).threeNum)"]["value"]["Element_001"]["contentStr"]["Element_000"]["contentStr"].stringValue,
+                                  secondSetEffect: self["Element_\((elementNum + 2).threeNum)"]["value"]["Element_002"]["contentStr"]["Element_000"]["contentStr"].stringValue,
+                                  thirdSetEffect: self["Element_\((elementNum + 2).threeNum)"]["value"]["Element_003"]["contentStr"]["Element_000"]["contentStr"].stringValue,
+                                  imageURL: self["Element_001"]["value"]["slotData"]["iconPath"].stringValue)
+        
+        return equipment
     }
 }
+
+
