fa8da691968110a3a4800fc49e46102752cf7a8c
doogie97 <chris_97@naver.com>
2022-07-14 17:26:59 +0900

    💎feat : 추가한 font사용하기 쉽도록 메서드 설정

--- LOARANG-MVVM/LOARANG-MVVM/Utils/UIFont + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/UIFont + Extension.swift
@@ -5,4 +5,18 @@
 //  Created by 최최성균 on 2022/07/14.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    enum Family: String {
+        case Bold, Light, Regular, Title
+    }
+    
+    static func one(size: CGFloat, family: Family) -> UIFont {
+        return UIFont(name: "ONEMobile\(family)", size: size) ?? UIFont()
+    }
+    
+    static func BlackHanSans(size: CGFloat) -> UIFont {
+        return UIFont(name: "BlackHanSans-Regular", size: size) ?? UIFont()
+    }
+}
