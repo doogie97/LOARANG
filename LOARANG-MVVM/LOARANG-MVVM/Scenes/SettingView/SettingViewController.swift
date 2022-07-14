509284505035ec435f57a0b20e998af832eb4520
doogie97 <chris_97@naver.com>
2022-07-14 23:22:27 +0900

    💎feat : SettingVC 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/SettingView/SettingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingViewController.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/07/14.
+//
+
+import UIKit
+
+final class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .systemBlue
+    }
+}
