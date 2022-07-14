08f24f5dfbef515d238d125d2947432283324d02
doogie97 <chris_97@naver.com>
2022-07-14 16:57:47 +0900

    💎feat : colorSet추가 (mainBackgroundColor)

---
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/UIColor + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor + Extension.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/07/14.
+//
+
+import UIKit
+
+extension UIColor {
+    class var mainBackground: UIColor? { return UIColor(named: "mainBackground") }
+}
