1709ab6ea8d7572e07a4aaefd8bdaf25e0d9b8ae
doogie97 <chris_97@naver.com>
2022-07-15 00:56:29 +0900

    💎feat : MockStorage 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/MockStorage.swift
@@ -0,0 +1,35 @@
+//
+//  MockStorage.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/07/15.
+//
+
+import RxRelay
+
+protocol Storageable {
+    var mainUser: BehaviorRelay<UserInfo> { get }
+    var bookMark: BehaviorRelay<[UserInfo]> { get }
+    func addUser(_ user: UserInfo)
+    func deleteUser(_ user: UserInfo)
+    func changeMainUser(_ user: UserInfo)
+}
+
+final class MockStorage: Storageable {
+    lazy var mainUser: BehaviorRelay<UserInfo> = BehaviorRelay(value: fakeUser().user)
+    lazy var bookMark = BehaviorRelay<[UserInfo]>(value: [])
+    
+    func addUser(_ user: UserInfo) {
+        bookMark.accept(bookMark.value + [user])
+    }
+    
+    func deleteUser(_ user: UserInfo) {
+        bookMark.accept(bookMark.value.filter { user.basicInfo.name != $0.basicInfo.name })
+    }
+    
+    func changeMainUser(_ user: UserInfo) {
+        mainUser.accept(user)
+    }
+    
+    
+}
