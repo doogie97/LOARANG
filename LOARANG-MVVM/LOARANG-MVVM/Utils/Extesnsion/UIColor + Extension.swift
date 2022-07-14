51b5efba4c497d9bf8d4385b2302a3e860ee49d5
doogie97 <chris_97@naver.com>
2022-07-15 04:07:22 +0900

    ✏️chore : colorset 그룹화 및 cell color 추가

--- LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIColor + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIColor + Extension.swift
@@ -11,4 +11,6 @@ extension UIColor {
     class var mainBackground: UIColor? { return UIColor(named: "mainBackground") }
     class var buttonColor: UIColor? { return UIColor(named: "buttonColor") }
     class var tableViewColor: UIColor? { return UIColor(named: "tableViewColor") }
+    class var cellBackgroundColor: UIColor? { return UIColor(named: "cellBackgroundColor") }
+    class var cellColor: UIColor? { return UIColor(named: "cellColor") }
 }
