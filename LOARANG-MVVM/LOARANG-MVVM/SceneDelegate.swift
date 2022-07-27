962bc57ef0dbfdcfdbb1e03efeef785981174938
doogie97 <chris_97@naver.com>
2022-07-27 17:21:13 +0900

    ♻️refactor : navigationController와 TabBarVC 포함관계 변경

--- LOARANG-MVVM/LOARANG-MVVM/SceneDelegate.swift
+++ LOARANG-MVVM/LOARANG-MVVM/SceneDelegate.swift
@@ -9,13 +9,15 @@ import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    let container = Container(storage: MockStorage())
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        
-        window?.rootViewController = TabBarViewController()
+        let navigationController = UINavigationController(rootViewController: TabBarViewController(container))
+        navigationController.isNavigationBarHidden = true
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
     }
 }
--- LOARANG-MVVM/LOARANG-MVVM/Scenes/TabBarViewController.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/TabBarViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 final class TabBarViewController: UITabBarController {
-    private let container = Container(storage: MockStorage())
-
+    private let container: Container
+    
+    init(_ container: Container) {
+        self.container = container
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setTabBarView()        
+        setTabBarView()
     }
     
     private func setTabBarView() {
@@ -20,16 +29,16 @@ final class TabBarViewController: UITabBarController {
         self.setViewControllers([mainVC, settingVC], animated: false)
     }
     
-    private lazy var mainVC: UINavigationController = {
-        let mainVC = UINavigationController(rootViewController: container.makeMainViewController())
+    private lazy var mainVC: UIViewController = {
+        let mainVC = container.makeMainViewController()
         mainVC.title = "홈"
         mainVC.tabBarItem.image = UIImage(systemName: "house.fill")
         
         return mainVC
     }()
     
-    private lazy var settingVC: UINavigationController = {
-        let settingVC = UINavigationController(rootViewController: SettingViewController())
+    private lazy var settingVC: UIViewController = {
+        let settingVC = SettingViewController()
         settingVC.title = "설정"
         settingVC.tabBarItem.image = UIImage(systemName: "gearshape.fill")
         
