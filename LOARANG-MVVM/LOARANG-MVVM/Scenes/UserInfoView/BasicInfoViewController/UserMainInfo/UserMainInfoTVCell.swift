6f03a62afac3d2ccd9a1b36257bd6f68a567a490
doogie97 <chris_97@naver.com>
2022-08-01 23:17:31 +0900

    💎feat : UserMainInfoCell 프로필 이미지, 클래스 label 구현

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoViewController/UserMainInfo/UserMainInfoTVCell.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoViewController/UserMainInfo/UserMainInfoTVCell.swift
@@ -25,14 +25,44 @@ final class UserMainInfoTVCell: UITableViewCell {
     }()
     
     private lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [])
+        let stackView = UIStackView(arrangedSubviews: [UIView(),imageClassStackView])
         stackView.backgroundColor = .cellColor
         stackView.layer.cornerRadius = 10
         
         return stackView
     }()
     
+    // 캐릭터 이미지 + 클래스명 스택뷰
+    private lazy var imageClassStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [UIView(),
+                                                       userImageView,
+                                                       classLabel])
+        stackView.axis = .vertical
+        
+        return stackView
+    }()
+    
+    private lazy var userImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "최지근")?.cropImage(class: "블레이드") //test code
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = UIScreen.main.bounds.width / 7
+        return imageView
+    }()
+    
+    private lazy var classLabel = makeLabel(size: 15, alignment: .center)
+    
+    private func makeLabel(size: Int, alignment: NSTextAlignment) -> UILabel {
+        let label = UILabel()
+        label.font = UIFont.one(size: size, family: .Bold)
+        label.textAlignment = alignment
+        label.textColor = .label
+        
+        return label
+    }
+    
     private func setLayout() {
+        self.selectionStyle = .none
         self.backgroundColor = .tableViewColor
         self.contentView.addSubview(backgourndView)
         self.backgourndView.addSubview(mainStackView)
@@ -45,5 +75,24 @@ final class UserMainInfoTVCell: UITableViewCell {
         mainStackView.snp.makeConstraints {
             $0.edges.equalToSuperview().inset(8)
         }
+        setLeftStackView()
+    }
+    
+    private func setLeftStackView() {
+        imageClassStackView.snp.makeConstraints {
+            $0.width.equalToSuperview().multipliedBy(0.3)
+        }
+        
+        userImageView.snp.makeConstraints {
+            $0.width.equalTo(userImageView.snp.height)
+        }
+
+        classLabel.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(0.3)
+        }
+    }
+    
+    func setCellContents(_ userInfo: UserInfo) {
+        self.classLabel.text = userInfo.basicInfo.class
     }
 }
