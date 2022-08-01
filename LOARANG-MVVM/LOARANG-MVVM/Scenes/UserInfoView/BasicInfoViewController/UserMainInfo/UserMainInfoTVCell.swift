7adb632dacf768223ffe5f05b80c35996cc1b396
doogie97 <chris_97@naver.com>
2022-08-02 00:48:27 +0900

    💎feat : UserMainInfoView 구성 완료

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoViewController/UserMainInfo/UserMainInfoTVCell.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoViewController/UserMainInfo/UserMainInfoTVCell.swift
@@ -17,6 +17,8 @@ final class UserMainInfoTVCell: UITableViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private let mainFontSize = 13
+    
     private lazy var backgourndView: UIView = {
         let view = UIView()
         view.backgroundColor = .cellBackgroundColor
@@ -25,14 +27,21 @@ final class UserMainInfoTVCell: UITableViewCell {
     }()
     
     private lazy var mainStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [UIView(),imageClassStackView])
+        let emptyView = UIView()
+        let stackView = UIStackView(arrangedSubviews: [emptyView, imageClassStackView, mainInfoStackView])
+        stackView.spacing = 10
         stackView.backgroundColor = .cellColor
         stackView.layer.cornerRadius = 10
+
+        
+        emptyView.snp.makeConstraints {
+            $0.width.equalTo(1)
+        }
         
         return stackView
     }()
     
-    // 캐릭터 이미지 + 클래스명 스택뷰
+    //MARK: - 캐릭터 이미지 + 클래스명 스택뷰
     private lazy var imageClassStackView: UIStackView = {
         let stackView = UIStackView(arrangedSubviews: [UIView(),
                                                        userImageView,
@@ -50,7 +59,7 @@ final class UserMainInfoTVCell: UITableViewCell {
         return imageView
     }()
     
-    private lazy var classLabel = makeLabel(size: 15, alignment: .center)
+    private lazy var classLabel = makeLabel(size: mainFontSize, alignment: .center)
     
     private func makeLabel(size: Int, alignment: NSTextAlignment) -> UILabel {
         let label = UILabel()
@@ -60,6 +69,66 @@ final class UserMainInfoTVCell: UITableViewCell {
         
         return label
     }
+    
+    //MARK: - 기본 정보 스택뷰
+    
+    private lazy var mainInfoStackView: UIStackView = {
+        let topEmptyView = UIView()
+        let bottomEmptyView = UIView()
+        
+        let stackView = UIStackView(arrangedSubviews: [topEmptyView,
+                                                       serverNameLabel,
+                                                       bottomStackView,
+                                                       bottomEmptyView])
+        stackView.axis = .vertical
+        
+        topEmptyView.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(0.04)
+        }
+        
+        bottomEmptyView.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(0.07)
+        }
+        
+        return stackView
+    }()
+    
+    private lazy var serverNameLabel = makeLabel(size: 16, alignment: .left)
+    
+    private lazy var bottomStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [itemPVPStackView,
+                                                       expeditionBattleLvStackView,
+                                                       titleLabel,
+                                                       guildLabel,
+                                                       wisdomLabel])
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+    
+    private lazy var itemPVPStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [itemLvLabel, pvpLabel])
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    private lazy var itemLvLabel = makeLabel(size: mainFontSize, alignment: .left)
+    private lazy var pvpLabel = makeLabel(size: mainFontSize, alignment: .left)
+    
+    private lazy var expeditionBattleLvStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [expeditionLvLabel, battleLvLabel])
+        stackView.distribution = .fillEqually
+        
+        return stackView
+    }()
+    
+    private lazy var expeditionLvLabel = makeLabel(size: mainFontSize, alignment: .left)
+    private lazy var battleLvLabel = makeLabel(size: mainFontSize, alignment: .left)
+    
+    private lazy var titleLabel = makeLabel(size: mainFontSize, alignment: .left)
+    private lazy var guildLabel = makeLabel(size: mainFontSize, alignment: .left)
+    private lazy var wisdomLabel = makeLabel(size: mainFontSize, alignment: .left)
     
     private func setLayout() {
         self.selectionStyle = .none
@@ -76,6 +145,7 @@ final class UserMainInfoTVCell: UITableViewCell {
             $0.edges.equalToSuperview().inset(8)
         }
         setLeftStackView()
+        setRightStackView()
     }
     
     private func setLeftStackView() {
@@ -92,7 +162,23 @@ final class UserMainInfoTVCell: UITableViewCell {
         }
     }
     
+    private func setRightStackView() {
+        serverNameLabel.snp.makeConstraints {
+            $0.height.equalToSuperview().multipliedBy(0.2)
+        }
+    }
+    
     func setCellContents(_ userInfo: UserInfo) {
-        self.classLabel.text = userInfo.basicInfo.class
+        let basicInfo = userInfo.basicInfo
+        classLabel.text = basicInfo.`class`
+        serverNameLabel.text = "@\(basicInfo.server) \(basicInfo.name)"
+        itemLvLabel.text = "아이템 : \(basicInfo.itemLV)"
+        pvpLabel.text = "PVP : \(basicInfo.pvp)"
+        expeditionLvLabel.text = "원정대 : \(basicInfo.expeditionLV)"
+        battleLvLabel.text = "전투 레벨 : \(basicInfo.battleLV)"
+        titleLabel.text = "칭호 : \(basicInfo.title)"
+        guildLabel.text = "길드 : \(basicInfo.guild)"
+        wisdomLabel.text = "영지 : \(basicInfo.wisdom)"
+        
     }
 }
