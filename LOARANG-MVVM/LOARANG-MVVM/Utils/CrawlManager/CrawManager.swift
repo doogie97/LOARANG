2c0be96f006717c6589aa5be1ab5f96d2b24b4ce
doogie97 <chris_97@naver.com>
2022-08-02 11:49:51 +0900

    💎feat : basicInfo 검색 기능 구현

--- LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawManager.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawManager.swift
@@ -15,21 +15,74 @@ struct CrawlManager: CrawlManagerable {
     private let baseURL = "https://m-lostark.game.onstove.com/Profile/Character/"
     
     func getUserInfo(_ name: String, completion: (Result<UserInfo, Error>) -> Void) {
-        guard let url = URL(string: baseURL + name.changeToPercent()) else {
+        guard let basicInfo = try? getBasicInfo(name: name) else {
             completion(.failure(CrawlError.searchError))
             return
         }
         
+        completion(.success(UserInfo(basicInfo: basicInfo,
+                                     stat: nil,
+                                     equips: nil)))
+    }
+    
+    private func makeURL(urlString: String, name: String) -> URL? {
+        guard let url = URL(string: urlString + name.changeToPercent()) else {
+            return nil
+        }
+        
+        return url
+    }
+    
+    private func makeDocument(url: URL) throws -> Document {
         guard let html = try? String(contentsOf: url, encoding: .utf8) else {
-            completion(.failure(CrawlError.searchError))
-            return
+            throw CrawlError.searchError
         }
         
         guard let doc = try? SwiftSoup.parse(html) else {
-            completion(.failure(CrawlError.searchError))
-            return
+            throw CrawlError.searchError
         }
         
-        print(url)
+        return doc
+    }
+    
+    //MARK: - basic info
+    private func getBasicInfo(name: String) throws -> BasicInfo {
+        guard let url = makeURL(urlString: baseURL, name: name) else {
+            throw CrawlError.searchError
+        }
+        
+        guard let doc = try? makeDocument(url: url) else {
+            throw CrawlError.searchError
+        }
+        
+        let server = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__user > dl.myinfo__user-names > dd > div.wrapper-define > dl:nth-child(1) > dd").text()
+        let name = name
+        let battleLV = try doc.select("#myinfo__character--button2 > span").text()
+        let itemLV = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(2) > dl.define.item > dd").text()
+        let expeditionLV = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(1) > dl:nth-child(1) > dd").text()
+        let title = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(1) > dl:nth-child(2) > dd").text()
+        let guild = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(3) > dl:nth-child(1) > dd").text()
+        let pvp = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(3) > dl:nth-child(2) > dd").text()
+        let wisdom = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(4) > dl > dd").text()
+        let `class` = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__user > dl.myinfo__user-names > dd > div.wrapper-define > dl:nth-child(2) > dd").text()
+        let userImageURL = getUserImageURL(name: name)
+        
+        return BasicInfo(server: server, name: name, battleLV: battleLV, itemLV: itemLV, expeditionLV: expeditionLV, title: title, guild: guild, pvp: pvp, wisdom: wisdom, class: `class`, userImageURL: userImageURL)
+    }
+    
+    private func getUserImageURL(name: String) -> String {
+        let urlString = "https://lostark.game.onstove.com/Profile/Character/"
+        guard let url = makeURL(urlString: urlString, name: name) else {
+            return ""
+        }
+        
+        guard let doc = try? makeDocument(url: url) else {
+            return ""
+        }
+        
+        let imageURL = try? doc.select("#profile-equipment > div.profile-equipment__character > img").attr("src")
+        return imageURL ?? ""
     }
 }
+
+
