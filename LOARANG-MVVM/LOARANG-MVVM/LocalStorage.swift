ccfaaff938d9f85ef5e0e616de30a8610f160fd9
doogie97 <chris_97@naver.com>
2022-08-03 00:10:43 +0900

    💎feat : LocalStorage read 및 북마크 add 기능 구현

--- LOARANG-MVVM/LOARANG-MVVM/LocalStorage.swift
+++ LOARANG-MVVM/LOARANG-MVVM/LocalStorage.swift
@@ -13,4 +13,65 @@ final class LocalStorage {
     init(realm: Realm) {
         self.realm = realm
     }
+    
+    func mainUser() -> MainUser? {
+        guard let mainUserDTO = realm.objects(LocalStorageModel.self).first?.mainUser else {
+            return nil
+        }
+        
+        return MainUser(image: UIImage(data: mainUserDTO.imageData) ?? UIImage(),
+                        battleLV: mainUserDTO.battleLV,
+                        name: mainUserDTO.name,
+                        class: mainUserDTO.`class`,
+                        itemLV: mainUserDTO.itemLV,
+                        server: mainUserDTO.server)
+    }
+    
+    func bookmarkUsers() -> [BookmarkUser] {
+        guard let bookmarkList = realm.objects(LocalStorageModel.self).first?.bookmarkUsers else {
+            return []
+        }
+        
+        let bookmarkUsers: [BookmarkUser] = bookmarkList.map {
+            return BookmarkUser(name: $0.name,
+                                image: UIImage(data: $0.imageData) ?? UIImage(),
+                                class: $0.`class`)
+        }
+        
+        return bookmarkUsers
+    }
+    
+    func addUser(_ user: BookmarkUser) throws {
+        do {
+            if realm.objects(LocalStorageModel.self).isEmpty {
+                let bookmarkList = LocalStorageModel().bookmarkUsers
+                
+                bookmarkList.append(user.convertedInfo)
+                
+                try realm.write {
+                    realm.add(bookmarkList)
+                }
+            } else {
+                try realm.write {
+                    guard let bookmarkList = realm.objects(LocalStorageModel.self).first?.bookmarkUsers else {
+                        return
+                    }
+                    bookmarkList.append(user.convertedInfo)
+                }
+            }
+        } catch {}
+
+    }
+    
+    func deleteUser(_ name: String) {
+        
+    }
+    
+    func changeMainUser(_ user: MainUser) {
+        
+    }
+    
+    func isBookmarkUser(_ name: String) -> Bool {
+        return true
+    }
 }
