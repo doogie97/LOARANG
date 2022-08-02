2eb82cee842f7bee7c6b0d8a9081624d89361570
doogie97 <chris_97@naver.com>
2022-08-02 10:39:00 +0900

    💎feat : 크롤링을 위한 기본 세팅 메서드 구현

--- LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawManager.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawManager.swift
@@ -5,14 +5,31 @@
 //  Created by 최최성균 on 2022/07/15.
 //
 
-import Foundation
+import SwiftSoup
 
 protocol CrawlManagerable {
     func getUserInfo(_ name: String, completion: (Result<UserInfo, Error>) -> Void)
 }
 
 struct CrawlManager: CrawlManagerable {
+    private let baseURL = "https://m-lostark.game.onstove.com/Profile/Character/"
+    
     func getUserInfo(_ name: String, completion: (Result<UserInfo, Error>) -> Void) {
-
+        guard let url = URL(string: baseURL + name.changeToPercent()) else {
+            completion(.failure(CrawlError.searchError))
+            return
+        }
+        
+        guard let html = try? String(contentsOf: url, encoding: .utf8) else {
+            completion(.failure(CrawlError.searchError))
+            return
+        }
+        
+        guard let doc = try? SwiftSoup.parse(html) else {
+            completion(.failure(CrawlError.searchError))
+            return
+        }
+        
+        print(url)
     }
 }
