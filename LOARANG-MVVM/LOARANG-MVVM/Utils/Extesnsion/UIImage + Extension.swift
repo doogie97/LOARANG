1f3aba4122d4bf5914a24047f2701fc0d6c29467
doogie97 <chris_97@naver.com>
2022-08-02 12:50:54 +0900

    ✏️chore : 이미지 crop위치 수정
    탈 쓴 캐릭터들 화면을 넘어가는 현상 때문에 수정

--- LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIImage + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIImage + Extension.swift
@@ -14,7 +14,7 @@ extension UIImage {
         if `class` == "기상술사" || `class` == "도화가" || `class` == "스페셜리스트" {
             cropZone = CGRect(x: 220, y: 180, width: 180, height: 180)
         } else {
-            cropZone = CGRect(x: 220, y: 80, width: 180, height: 180)
+            cropZone = CGRect(x: 210, y: 60, width: 200, height: 200)
         }
         
         guard let cutImageRef = self.cgImage?.cropping(to: cropZone) else {
