e87cbff6dc0c5a02efd719f719185af9b0a8aeb2
doogie97 <chris_97@naver.com>
2022-08-03 01:01:51 +0900

    💎feat : alert 메세지가 nil일 경우 기본 값 구현

--- LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIViewController + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIViewController + Extension.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIViewController {
     func showAlert(title: String?, message: String?) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title: title , message: message ?? "알 수 없는 오류가 발생하였습니다", preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default)
         
         alert.addAction(action)
