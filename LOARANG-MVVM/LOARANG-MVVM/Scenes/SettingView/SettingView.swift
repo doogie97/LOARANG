6ab5516746367130b04ecf7622594f851a033e53
doogie97 <chris_97@naver.com>
2022-08-10 12:45:43 +0900

    💎feat : settingView 구성

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/SettingView/SettingView.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/SettingView/SettingView.swift
@@ -18,7 +18,36 @@ final class SettingView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "설정"
+        
+        label.font = UIFont.one(size: 30, family: .Bold)
+        
+        return label
+    }()
+    
+    private(set) lazy var menuTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .tableViewColor
+        tableView.separatorStyle = .none
+        
+        tableView.register(SettingTVCell.self)
+        
+        return tableView
+    }()
+    
     private func setLayout() {
-        self.backgroundColor = .red
+        self.backgroundColor = .mainBackground
+        self.addSubview(titleLabel)
+        self.addSubview(menuTableView)
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(20)
+        }
+        menuTableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).inset(-10)
+            $0.bottom.leading.trailing.equalTo(self.safeAreaLayoutGuide)
+        }
     }
 }
--- LOARANG-MVVM/LOARANG-MVVM/Scenes/SettingView/SettingViewController.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/SettingView/SettingViewController.swift
@@ -12,5 +12,52 @@ final class SettingViewController: UIViewController {
     override func loadView() {
         super.loadView()
         self.view = settingView
+        setTableView()
+    }
+    
+    private func setTableView() {
+        settingView.menuTableView.dataSource = self
+        settingView.menuTableView.delegate = self
+    }
+}
+
+extension SettingViewController: UITableViewDataSource {
+    enum CellType: Int, CaseIterable {
+        case changeMainUser = 0
+        case notice = 1
+        case suggestion = 2
+        
+        var title: String {
+            switch self {
+            case .changeMainUser:
+                return "대표 캐릭터 변경"
+            case .notice:
+                return "공지 사항"
+            case .suggestion:
+                return "버그제보 및 건의"
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        CellType.allCases.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "\(SettingTVCell.self)", for: indexPath) as? SettingTVCell else {
+            return UITableViewCell()
+        }
+        guard let title = CellType(rawValue: indexPath.row)?.title else {
+            return UITableViewCell()
+        }
+        cell.setCellContents(title: title)
+        
+        return cell
+    }
+}
+
+extension SettingViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(CellType(rawValue: indexPath.row)?.title)
     }
 }
