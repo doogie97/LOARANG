3ff32ac56c9e4d8152d9348894360ef253f41bd6
doogie97 <chris_97@naver.com>
2022-08-11 14:25:08 +0900

    💎feat : 세로모드만 지원하도록 수정

--- LOARANG-MVVM/LOARANG-MVVM/AppDelegate.swift
+++ LOARANG-MVVM/LOARANG-MVVM/AppDelegate.swift
@@ -18,5 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+           
+           // 세로모드 고정
+           return UIInterfaceOrientationMask.portrait
+       }
 }
 
