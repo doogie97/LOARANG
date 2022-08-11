9026eb713ad3348bcc50050c5c76cd10b89ebdcf
doogie97 <chris_97@naver.com>
2022-08-11 11:19:15 +0900

    ✏️chore : 오류 문구 수정

--- LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIViewController + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/UIViewController + Extension.swift
@@ -9,7 +9,7 @@ import UIKit
 
 extension UIViewController {
     func showAlert(title: String?, message: String?) {
-        let alert = UIAlertController(title: title , message: message ?? "알 수 없는 오류가 발생하였습니다", preferredStyle: .alert)
+        let alert = UIAlertController(title: title , message: message ?? "알 수 없는 오류가 발생하였습니다\n 잠시 후 다시 시도해 주세요", preferredStyle: .alert)
         let action = UIAlertAction(title: "확인", style: .default)
         
         alert.addAction(action)
