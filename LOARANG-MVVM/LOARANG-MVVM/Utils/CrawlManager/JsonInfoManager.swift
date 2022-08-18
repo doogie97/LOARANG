31e90a6f9f1c7dc7a47c8a008d1d6d322a0fb3c7
doogie97 <chris_97@naver.com>
2022-08-18 14:27:13 +0900

    💎feat : JsonInfoManager 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/JsonInfoManager.swift
@@ -0,0 +1,14 @@
+//
+//  JsonInfoManager.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/08/18.
+//
+
+struct JsonInfoManager {
+    private let jsonString: String
+    
+    init(jsonString: String) {
+        self.jsonString = jsonString
+    }
+}
