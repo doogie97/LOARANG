f1dde59e8f5c4b50d78f6522f701dd8f4ea35485
doogie97 <chris_97@naver.com>
2022-08-20 22:53:03 +0900

    🔨fix : 품질 99가 빨간색 막대로 표시되던 현상 수정

--- LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/Int + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/Int + Extension.swift
@@ -16,7 +16,7 @@ extension Int {
             return #colorLiteral(red: 0.561450541, green: 0.9948626161, blue: 0, alpha: 1)
         case 70..<89:
             return #colorLiteral(red: 0.007898607291, green: 0.7087070346, blue: 1, alpha: 1)
-        case 90..<99:
+        case 90...99:
             return #colorLiteral(red: 0.8060045242, green: 0.261687547, blue: 0.9900844693, alpha: 1)
         case 100:
             return #colorLiteral(red: 0.9972185493, green: 0.5881507397, blue: 0.007285744417, alpha: 1)
