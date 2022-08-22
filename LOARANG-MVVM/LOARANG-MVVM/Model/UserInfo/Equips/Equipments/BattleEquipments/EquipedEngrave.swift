ead72eb412adeed9604f7124dbc49803b7c619b1
doogie97 <chris_97@naver.com>
2022-08-22 13:15:27 +0900

    ♻️refactor : 활성화 수치에 따른 title 색상 추가

--- LOARANG-MVVM/LOARANG-MVVM/Model/UserInfo/Equips/Equipments/BattleEquipments/EquipedEngrave.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Model/UserInfo/Equips/Equipments/BattleEquipments/EquipedEngrave.swift
@@ -5,8 +5,25 @@
 //  Created by 최최성균 on 2022/08/22.
 //
 
+import UIKit
+
 struct EquipedEngrave {
     let name: String
     let activation: Int
     let imageURL: String
+    
+    var titleColor: UIColor {
+        switch activation {
+        case 3..<6:
+            return #colorLiteral(red: 0.5529411765, green: 0.9764705882, blue: 0.003921568627, alpha: 1)
+        case 6..<9:
+            return #colorLiteral(red: 0, green: 0.6901960784, blue: 0.9803921569, alpha: 1)
+        case 9..<12:
+            return #colorLiteral(red: 0.8078431373, green: 0.262745098, blue: 0.9882352941, alpha: 1)
+        case 12:
+            return #colorLiteral(red: 0.9764705882, green: 0.5725490196, blue: 0, alpha: 1)
+        default:
+            return .label
+        }
+    }
 }
