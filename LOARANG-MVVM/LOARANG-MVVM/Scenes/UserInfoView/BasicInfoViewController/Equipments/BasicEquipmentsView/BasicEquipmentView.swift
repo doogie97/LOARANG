184cdd7d03a90fb0861dde4d16fe26f8fe4e3e90
doogie97 <chris_97@naver.com>
2022-08-22 18:56:09 +0900

    💎feat : 보석 표시할 view layout 구성

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoViewController/Equipments/BasicEquipmentsView/BasicEquipmentView.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoViewController/Equipments/BasicEquipmentsView/BasicEquipmentView.swift
@@ -44,28 +44,63 @@ final class BasicEquipmentView: UIView {
         return tableView
     }()
     
-    private lazy var jewelryStackView: UIStackView = {
+    private lazy var gemView: UIView = {
+        let view = UIView()
+        
+        return view
+    }()
+    
+    private lazy var gemTitleLabel: UILabel = {
         let label = UILabel()
-        label.text = "나중에 보석 들어올 스택뷰"
+        label.text = "보석"
+        label.font = .one(size: 18, family: .Bold)
         
-        let stackView = UIStackView(arrangedSubviews: [label])
+        return label
+    }()
+    
+    private lazy var gemCollectionView: UICollectionView = { //임시 레이아웃
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalHeight(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
         
-        return stackView
+        let groupSize = NSCollectionLayoutSize(widthDimension: itemSize.widthDimension, heightDimension: itemSize.heightDimension)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .red
+        collectionView.register(BookmarkCVCell.self, forCellWithReuseIdentifier: "\(BookmarkCVCell.self)")
+
+        return collectionView
     }()
     
     private func setLayout() {
         self.addSubview(mainStackView)
-        self.addSubview(jewelryStackView)
+        self.addSubview(gemView)
+        gemView.addSubview(gemTitleLabel)
+        gemView.addSubview(gemCollectionView)
         
         mainStackView.snp.makeConstraints {
             $0.top.equalTo(safeAreaLayoutGuide).inset(10)
             $0.leading.trailing.equalTo(safeAreaLayoutGuide).inset(5)
-            $0.bottom.equalTo(jewelryStackView.snp.top)
+            $0.bottom.equalTo(gemView.snp.top)
         }
         
-        jewelryStackView.snp.makeConstraints {
+        gemView.snp.makeConstraints {
             $0.height.equalToSuperview().dividedBy(5)
             $0.trailing.leading.bottom.equalToSuperview()
+        }
+        
+        gemTitleLabel.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(10)
+        }
+        
+        gemCollectionView.snp.makeConstraints {
+            $0.top.equalTo(gemTitleLabel.snp.bottom)
+            $0.leading.bottom.trailing.equalToSuperview().inset(5)
         }
     }
 }
