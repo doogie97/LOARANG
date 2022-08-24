e2d550d192bfc3992726c867e613e58fb39468e1
doogie97 <chris_97@naver.com>
2022-08-24 13:47:43 +0900

    💎feat : Skill Model 생성

--- LOARANG-MVVM/LOARANG-MVVM/Model/UserInfo/UserJsonInfos/SkillInfo/Skill.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Model/UserInfo/UserJsonInfos/SkillInfo/Skill.swift
@@ -6,5 +6,23 @@
 //
 
 struct Skill {
-    
+    let name: String
+    let coolTime: String
+    let actionType: String
+    let skillType: String
+    let imageURL: String
+    let battleType: String
+    let skillLv: String
+    let cost: String
+    let skillDescription: String
+    let tripods: [Tripod]
+    let runeEffect: String
+    let gemEffect: String
+}
+
+struct Tripod {
+    let name: String
+    let description: String
+    let lv: String
+    let imageURL: String
 }
