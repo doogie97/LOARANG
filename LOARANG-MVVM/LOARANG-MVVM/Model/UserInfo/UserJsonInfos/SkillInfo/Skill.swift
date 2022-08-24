2e6b2acf4168f9706ce3c07f5ed204e9f2f8e30f
doogie97 <chris_97@naver.com>
2022-08-24 12:40:29 +0900

    💎feat : SkillInfo Model 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/Model/UserInfo/UserJsonInfos/SkillInfo/Skill.swift
@@ -0,0 +1,10 @@
+//
+//  Skill.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/08/24.
+//
+
+struct Skill {
+    
+}
---
+++ LOARANG-MVVM/LOARANG-MVVM/Model/UserInfo/UserJsonInfos/SkillInfo/SkillInfo.swift
@@ -0,0 +1,12 @@
+//
+//  SkillInfo.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/08/24.
+//
+
+struct SkillInfo {
+    let usedSkillPoint: String
+    let totalSkillPoint: String
+    let skills: [Skill]
+}
