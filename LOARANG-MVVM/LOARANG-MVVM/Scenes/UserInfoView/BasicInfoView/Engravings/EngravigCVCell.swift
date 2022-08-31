a0f3a148b4668ac00ceaa551c1246d4ba4cd97b9
doogie97 <chris_97@naver.com>
2022-08-31 13:16:53 +0900

    💎feat : EngravigTVCell 내부에 배치할 EngravigCVCell 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/BasicInfoView/Engravings/EngravigCVCell.swift
@@ -0,0 +1,38 @@
+//
+//  EngravigCVCell.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/08/31.
+//
+
+import SnapKit
+
+final class EngravigCVCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .one(size: 13, family: .Bold)
+        
+        return label
+    }()
+    
+    private func setLayout() {
+        self.contentView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func setCellContents(engraving: Engraving) {
+        self.titleLabel.text = engraving.title
+    }
+}
