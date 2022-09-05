e22eb88c84616ac4aba2cd119a90b66b53f51d49
doogie97 <chris_97@naver.com>
2022-09-05 18:37:13 +0900

    💎feat : CollectionInfoView 구성

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/CollectionInfoView/CollectionInfoView.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/CollectionInfoView/CollectionInfoView.swift
@@ -18,7 +18,43 @@ final class CollectionInfoView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private let menuScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.layer.cornerRadius = 5
+        scrollView.backgroundColor = #colorLiteral(red: 0.1659600362, green: 0.1790002988, blue: 0.1983416486, alpha: 1)
+        return scrollView
+    }()
+    
+    private(set) lazy var segmentControl: CustomSegmentControl = {
+        let segmentControl = CustomSegmentControl(segmentTitles: ["거인의 심장", "섬의 마음", "모코코 씨앗", "위대한 미술품", "항해 모험물", "세계수의 잎", "이그네아의 징표", "오르페우스의 별", "기억의 오르골"])
+        segmentControl.selectedFontColor = .label
+        segmentControl.selectedFont = .one(size: 13, family: .Bold)
+        segmentControl.deselectedFont = .one(size: 13, family: .Light)
+        segmentControl.underLineColor = nil
+        segmentControl.backColor = UIColor(white: 0.1, alpha: 0.5)
+        
+        return segmentControl
+    }()
+    
     private func setLayout() {
-        self.backgroundColor = .yellow
+        self.backgroundColor = .mainBackground
+        
+        self.addSubview(menuScrollView)
+        
+        menuScrollView.addSubview(segmentControl)
+        
+        menuScrollView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(8)
+            $0.height.equalTo(40)
+        }
+        
+        segmentControl.snp.makeConstraints {
+            $0.width.equalTo(UIScreen.main.bounds.width * 2.5)
+            $0.height.equalToSuperview()
+            $0.top.bottom.equalToSuperview()
+            $0.trailing.leading.equalToSuperview().inset(3)
+        }
     }
 }
