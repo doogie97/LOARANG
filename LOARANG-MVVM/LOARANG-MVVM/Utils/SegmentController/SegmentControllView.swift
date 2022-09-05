4e8407968a4ce5a4026c1446a98bd88a3cb1daa3
doogie97 <chris_97@naver.com>
2022-09-05 17:12:26 +0900

    💎feat : 콜렉션 뷰를 이용한 커스텀 Segmentcontrol 구현 #13

---
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/SegmentController/SegmentControllView.swift
@@ -0,0 +1,132 @@
+//
+//  SegmentControllView.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/09/05.
+//
+
+import SnapKit
+
+final class CustomSegmentControl: UIView {
+    private let segmentTitles: [String]
+    var selectedFontColor: UIColor = .label
+    var deselectedFontColor: UIColor = .systemGray
+    var selectedFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .bold)
+    var deselectedFont: UIFont = UIFont.systemFont(ofSize: 14, weight: .light)
+    var segmentTextAlignment: NSTextAlignment = .center
+    
+    init(segmentTitles: [String]) {
+        self.segmentTitles = segmentTitles
+        
+        super.init(frame: .zero)
+        setLayout()
+        segmentCollectionView.dataSource = self
+        segmentCollectionView.delegate = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private(set) lazy var segmentCollectionView: UICollectionView = {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: itemSize.widthDimension, heightDimension: itemSize.heightDimension)
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: segmentTitles.count)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.register(segCVCell.self, forCellWithReuseIdentifier: "\(segCVCell.self)")
+        
+        collectionView.isScrollEnabled = false
+
+        return collectionView
+    }()
+    
+    private lazy var underLineView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 1, green: 0.6752033234, blue: 0.5361486077, alpha: 1)
+        
+        return view
+    }()
+    
+    private func setLayout() {
+        self.addSubview(segmentCollectionView)
+        self.addSubview(underLineView)
+        
+        segmentCollectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        underLineView.snp.makeConstraints {
+            $0.top.equalTo(segmentCollectionView.snp.bottom)
+            $0.height.equalTo(2)
+            
+            $0.leading.equalTo(segmentCollectionView)
+            
+            $0.width.equalTo(segmentCollectionView.snp.width).dividedBy(segmentTitles.count)
+        }
+    }
+}
+
+extension CustomSegmentControl: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        self.segmentTitles.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "\(segCVCell.self)", for: indexPath) as? segCVCell else {
+            return UICollectionViewCell()
+        }
+        
+        if indexPath.row == 0 {
+            collectionView.selectItem(at: indexPath, animated: false , scrollPosition: .init())
+            cell.selectedCell(color: selectedFontColor, font: selectedFont)
+        } else {
+            cell.deselectedCell(color: deselectedFontColor, font: deselectedFont)
+        }
+        
+        cell.setCellContents(title: segmentTitles[indexPath.row], textAlignment: segmentTextAlignment)
+        return cell
+    }
+}
+
+extension CustomSegmentControl: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {        
+        guard let cell = collectionView.cellForItem(at: indexPath) as? segCVCell else {
+            return
+        }
+        cell.selectedCell(color: selectedFontColor, font: selectedFont)
+        changeUnderLinePosition(index: indexPath.row)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        guard let cell = collectionView.cellForItem(at: indexPath) as? segCVCell else {
+            return
+        }
+        cell.deselectedCell(color: deselectedFontColor, font: deselectedFont)
+    }
+    
+    private func changeUnderLinePosition(index: Int) {
+        let segmentIndex = CGFloat(index) //여기에 콜렉션 뷰 row전달하기
+        let segmentWidth = segmentCollectionView.frame.width / CGFloat(segmentTitles.count)
+        let leadingDistance = segmentWidth * segmentIndex
+
+        UIView.animate(withDuration: 0.2, animations: { [weak self] in
+            guard let self = self else {
+                return
+            }
+
+            self.underLineView.snp.updateConstraints {
+                $0.leading.equalTo(self.segmentCollectionView).inset(leadingDistance)
+            }
+            self.layoutIfNeeded()
+        })
+    }
+}
---
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/SegmentController/segmentCVCell.swift
@@ -0,0 +1,50 @@
+//
+//  segmentCVCell.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/09/05.
+//
+
+import SnapKit
+
+final class segCVCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        
+        return label
+    }()
+
+
+    private func setLayout() {
+        self.contentView.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    func setCellContents(title: String, textAlignment: NSTextAlignment) {
+        titleLabel.text = title
+        titleLabel.textAlignment = textAlignment
+    }
+    
+    func selectedCell(color: UIColor, font: UIFont) {
+        titleLabel.font = font
+        titleLabel.textColor = color
+    }
+    
+    func deselectedCell(color: UIColor, font: UIFont) {
+        titleLabel.font = font
+        titleLabel.textColor = color
+    }
+}
