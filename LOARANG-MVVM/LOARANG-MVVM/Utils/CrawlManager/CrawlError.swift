af17f5aa431f5d462dc23a5864d73c111c9617eb
doogie97 <chris_97@naver.com>
2022-09-16 13:00:24 +0900

    ♻️refactor : CrawlError에 ownCharacterErrror 추가

--- LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawlError.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawlError.swift
@@ -11,6 +11,7 @@ enum CrawlError: Error {
     case documentError
     case jsonInfoError
     case inspection
+    case ownCharacterErrror
     
     var errorDescription: String {
         return "유저 검색 중 오류가 발생했습니다\n(Error code: \(self))"
