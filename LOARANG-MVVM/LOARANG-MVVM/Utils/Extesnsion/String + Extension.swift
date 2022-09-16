5c1627c41be2cad79cb698f541d88f6f9bb7cb29
doogie97 <chris_97@naver.com>
2022-09-16 17:26:07 +0900

    💎feat : 클래스에 따라 이미지 가져오는 기능 구현

--- LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/String + Extension.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/Extesnsion/String + Extension.swift
@@ -68,4 +68,57 @@ extension String {
         
         return attrString
     }
+    
+    var classImage: UIImage? {
+        switch self {
+        case "디스트로이어":
+            return UIImage(named: "emblem_destroyer")
+        case "워로드":
+            return UIImage(named: "emblem_warlord")
+        case "버서커":
+            return UIImage(named: "emblem_berserker")
+        case "홀리나이트":
+            return UIImage(named: "emblem_holyknight")
+        case "스트라이커":
+            return UIImage(named: "emblem_battle_master_male")
+        case "배틀마스터":
+            return UIImage(named: "emblem_battle_master")
+        case "인파이터":
+            return UIImage(named: "emblem_infighter")
+        case "기공사":
+            return UIImage(named: "emblem_force_master")
+        case "창술사":
+            return UIImage(named: "emblem_lance_master")
+        case "데빌헌터":
+            return UIImage(named: "emblem_devil_hunter")
+        case "블래스터":
+            return UIImage(named: "emblem_blaster")
+        case "호크아이":
+            return UIImage(named: "emblem_hawk_eye")
+        case "스카우터":
+            return UIImage(named: "emblem_scouter")
+        case "건슬링어":
+            return UIImage(named: "emblem_devil_hunter_female")
+        case "바드":
+            return UIImage(named: "emblem_bard")
+        case "서머너":
+            return UIImage(named: "emblem_summoner")
+        case "아르카나":
+            return UIImage(named: "emblem_arcana")
+        case "소서리스":
+            return UIImage(named: "emblem_elemental_master")
+        case "블레이드":
+            return UIImage(named: "emblem_blade")
+        case "데모닉":
+            return UIImage(named: "emblem_demonic")
+        case "리퍼":
+            return UIImage(named: "emblem_reaper")
+        case "도화가":
+            return UIImage(named: "emblem_yinyangshi")
+        case "기상술사":
+            return UIImage(named: "emblem_weather_artist")
+        default:
+            return nil
+        }
+    }
 }
