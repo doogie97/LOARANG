b154fde4ac1a20a3ec72d163ee970eef653fc3bc
doogie97 <chris_97@naver.com>
2022-09-16 13:20:47 +0900

    💎feat : OwnCharacterViewModel output 정의

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/ForthInfoView/OwnCharacterViewModel.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/UserInfoView/ForthInfoView/OwnCharacterViewModel.swift
@@ -5,12 +5,36 @@
 //  Created by 최최성균 on 2022/09/16.
 //
 
+import RxRelay
+import RxSwift
+
 protocol OwnCharacterViewModelable: OwnCharacterViewModelInput, OwnCharacterViewModelOutput {}
 
 protocol OwnCharacterViewModelInput {}
 
-protocol OwnCharacterViewModelOutput {}
+protocol OwnCharacterViewModelOutput {
+    var ownCharacterInfo: BehaviorRelay<OwnCharacterInfo?> { get }
+    var ownCharacters: BehaviorRelay<[OwnCharacter]> { get }
+}
 
 final class OwnCharacterViewModel: OwnCharacterViewModelable {
+    private let disposeBag = DisposeBag()
+    init(ownCharacterInfo: BehaviorRelay<OwnCharacterInfo?>) {
+        self.ownCharacterInfo = ownCharacterInfo
+        bind()
+    }
     
+    private func bind() {
+        ownCharacterInfo.bind(onNext: { [weak self] in
+            guard let ownCharacterInfo = $0 else {
+                return
+            }
+            self?.ownCharacters.accept(ownCharacterInfo.ownCharacters)
+        })
+        .disposed(by: disposeBag)
+    }
+    
+    //out
+    let ownCharacterInfo: BehaviorRelay<OwnCharacterInfo?>
+    let ownCharacters = BehaviorRelay<[OwnCharacter]>(value: [])
 }
