c31b8217a2f4bc3518a8358ee554882c3ac56484
doogie97 <chris_97@naver.com>
2022-09-22 20:25:11 +0900

    💎feat : showEventPage Output 생성

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/Main/MainViewModel.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/Main/MainViewModel.swift
@@ -21,6 +21,7 @@ protocol MainViewModelOutput {
     var events: BehaviorRelay<[LostArkEvent]> { get }
     var showSearchView: PublishRelay<Void> { get }
     var showUserInfo: PublishRelay<String> { get }
+    var showEventPage: PublishRelay<String> { get }
 }
 
 final class MainViewModel: MainViewModelInOut {
@@ -65,7 +66,7 @@ final class MainViewModel: MainViewModelInOut {
             return
         }
         
-        print(eventURL)
+        showEventPage.accept(eventURL)
     }
     
     // out
@@ -74,4 +75,5 @@ final class MainViewModel: MainViewModelInOut {
     let events = BehaviorRelay<[LostArkEvent]>(value: [])
     let showSearchView = PublishRelay<Void>()
     let showUserInfo = PublishRelay<String>()
+    let showEventPage = PublishRelay<String>()
 }
