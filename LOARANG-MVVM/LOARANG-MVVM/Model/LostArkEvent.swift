f9b7964c93ceb5513d6303d6596aed4243a8a738
doogie97 <chris_97@naver.com>
2022-09-22 19:13:37 +0900

    💎feat : LostArkEvent 모델 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/Model/LostArkEvent.swift
@@ -0,0 +1,12 @@
+//
+//  LostArkEvent.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/09/22.
+//
+
+struct LostArkEvent {
+    let endDate: String
+    let imageURL: String
+    let eventURL: String
+}
