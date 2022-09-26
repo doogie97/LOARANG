6f7299b5e4961945207e880ca464c90257621162
doogie97 <chris_97@naver.com>
2022-09-26 11:42:57 +0900

    💎feat : WebViewViewModel에 URL Output 생성

--- LOARANG-MVVM/LOARANG-MVVM/WebView/WebViewViewModel.swift
+++ LOARANG-MVVM/LOARANG-MVVM/WebView/WebViewViewModel.swift
@@ -5,12 +5,21 @@
 //  Created by 최최성균 on 2022/09/22.
 //
 
+import Foundation
+
 protocol WebViewViewModelable: WebViewViewModelInput, WebViewViewModelOutput {}
 
 protocol WebViewViewModelInput {}
 
-protocol WebViewViewModelOutput {}
+protocol WebViewViewModelOutput {
+    var url: URL { get }
+}
 
 final class WebViewViewModel: WebViewViewModelable {
+    init(url: URL) {
+        self.url = url
+    }
     
+    //out
+    let url: URL
 }
