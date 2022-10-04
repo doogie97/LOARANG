cfed1fd37a7e02610f09303e03719906b677bc73
doogie97 <chris_97@naver.com>
2022-10-04 13:14:31 +0900

    💎feat : 저장소 오류 발생시 에러 얼럿 표시 후 종료 기능 구현

--- LOARANG-MVVM/LOARANG-MVVM/SceneDelegate.swift
+++ LOARANG-MVVM/LOARANG-MVVM/SceneDelegate.swift
@@ -11,17 +11,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let realm = try? Realm() else {
-            return
-        }
-        let container = Container(storage: AppStorage(LocalStorage(realm: realm)))
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        
+        guard let realm = try? Realm() else {
+            window?.rootViewController = UIViewController()
+            window?.rootViewController?.showExitAlert(message: "앱 저장소 오류가 발생했습니다.\n앱 재설치 혹은 고객센터로 문의 부탁드립니다.")
+            return
+        }
+        
+        let container = Container(storage: AppStorage(LocalStorage(realm: realm)))
         let navigationController = UINavigationController(rootViewController: TabBarViewController(container))
         navigationController.isNavigationBarHidden = true
         window?.rootViewController = navigationController
-        window?.makeKeyAndVisible()
     }
 }
 
