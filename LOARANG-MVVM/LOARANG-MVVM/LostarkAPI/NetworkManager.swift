17588367731d41bfe28b2c2991bd485cac6b4b37
doogie97 <chris_97@naver.com>
2022-12-11 17:56:30 +0900

    ♻️refactor : requset 메서드 responseDecodable로 수정

--- LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
+++ LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
@@ -5,7 +5,6 @@
 //  Created by 최최성균 on 2022/12/11.
 //
 
-import Foundation
 import Alamofire
 
 protocol NetworkManagerable {
@@ -14,55 +13,31 @@ protocol NetworkManagerable {
 
 struct NetworkManager: NetworkManagerable {
     func request<T: Decodable>(_ requestable: Requestable, resultType: T.Type, completion: @escaping (Result<T, APIError>) -> Void) {
-        do {
-            let request = try urlRequest(requestable)
-            
-            AF.request(request).responseDecodable(of: resultType.self) { result in
-                guard result.error == nil else {
-                    completion(.failure(APIError.transportError))
-                    return
-                }
-                
-                guard let response = result.response else {
-                    completion(.failure(APIError.responseError))
-                    return
-                }
-                
-                guard !(200...299).contains(response.statusCode) else {
-                    completion(.failure(APIError.statusCodeError))
-                    return
-                }
-                
-                guard let value = result.value else {
-                    completion(.failure(APIError.dataError))
-                    return
-                }
-                
-                completion(.success(value))
+        let requset = AF.request(requestable.baseURL + requestable.path,method: requestable.httpMethod, parameters: requestable.params, headers: HTTPHeaders(requestable.header))
+        
+        requset.responseDecodable(of: T.self) { result in
+            guard result.error == nil else {
+                completion(.failure(APIError.transportError))
+                return
             }
-        } catch let error {
-            completion(.failure(error as? APIError ?? APIError.unknownError))
+
+            guard let response = result.response else {
+                completion(.failure(APIError.responseError))
+                return
+            }
+
+            guard (200...299).contains(response.statusCode) else {
+                print(response.statusCode)
+                completion(.failure(APIError.statusCodeError))
+                return
+            }
+
+            guard let value = result.value else {
+                completion(.failure(APIError.dataError))
+                return
+            }
+            
+            completion(.success(value))
         }
-    }
-    
-    private func urlRequest(_ requestable: Requestable) throws -> URLRequest {
-        guard let url = URL(string: requestable.baseURL + requestable.path) else {
-            throw APIError.urlError
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = requestable.httpMethod.rawValue
-        
-        requestable.header.forEach { key, value in
-            request.addValue(value, forHTTPHeaderField: key)
-        }
-        
-        do {
-            request.httpBody = try JSONSerialization.data(withJSONObject: requestable.params, options: [])
-        } catch {
-            throw APIError.httpBodyError
-        }
-        
-        return request
     }
 }
