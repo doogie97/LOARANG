4107ca6b60e242a1d4254474dc487e7f60f67e7e
doogie97 <chris_97@naver.com>
2022-12-11 12:37:53 +0900

    💎feat : NetworkManager 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
@@ -0,0 +1,19 @@
+//
+//  NetworkManager.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/12/11.
+//
+
+import Foundation
+import Alamofire
+
+protocol NetworkManagerable {
+    func request()
+}
+
+struct NetworkManager: NetworkManagerable {
+    func request() {
+        
+    }
+}
