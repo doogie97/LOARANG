04411f84d973da07f54888407fb816c9acce71e1
doogie97 <chris_97@naver.com>
2022-12-11 13:28:27 +0900

    💎feat : News 모델 생성

--- LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/APIModels/NewsAPIModel.swift
+++ LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/APIModels/NewsAPIModel.swift
@@ -14,3 +14,23 @@ struct NewsAPIModel: Requestable {
     var params: [String : Any] = [:]
     var httpMethod = HTTPMethod.get
 }
+
+struct News: Decodable {
+    struct contents: Decodable {
+        let title: String?
+        let thumbnail: String?
+        let link: String?
+        let startDate: String?
+        let endDate: String?
+        let rewardDate: String?
+        
+        enum codingKeys: String, CodingKey {
+            case title = "Title"
+            case thumbnail = "Thumbnail"
+            case link = "Link"
+            case startDate = "StartDate"
+            case endDate = "EndDate"
+            case rewardDate = "RewardDate"
+        }
+    }
+}
