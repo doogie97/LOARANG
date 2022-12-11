f10f16138ec34d0f059574be727c6c4d0e653c92
doogie97 <chris_97@naver.com>
2022-12-11 22:52:35 +0900

    📱test : Characters request test케이스 생성

--- LOARANG-MVVM/LOARANG-MVVMTests/LOARANG_MVVMTests.swift
+++ LOARANG-MVVM/LOARANG-MVVMTests/LOARANG_MVVMTests.swift
@@ -30,4 +30,29 @@ final class LOARANG_MVVMTests: XCTestCase {
         }
         wait(for: [promise], timeout: 5)
     }
+    
+    func test_Requset호출시_Characters정보를_잘_가져오는지() {
+        //given
+        let promise = expectation(description: "Characters를 잘 가져오는지")
+        let networkManager = NetworkManager()
+        let api = NewsAPIModel()
+        
+        //when
+        NetworkManager().request(CharactersAPIModel(name: "최지근"), resultType: [CharacterInfo].self) { result in
+            switch result {
+            case .success(let characters):
+                guard let character = characters.first else {
+                    XCTFail()
+                    return
+                }
+                
+                XCTAssertEqual(character.characterName, "스고히")
+            case .failure(let error):
+                print(error)
+                XCTFail()
+            }
+            promise.fulfill()
+        }
+        wait(for: [promise], timeout: 5)
+    }
 }
