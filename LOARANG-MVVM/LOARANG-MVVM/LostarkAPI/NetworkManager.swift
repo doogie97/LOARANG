ce857c7abde78f0f2a9ad0ee8715e89a32b7aefb
doogie97 <chris_97@naver.com>
2022-12-11 13:14:21 +0900

    💎feat : urlRequest 생성 메서드 구현

--- LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
+++ LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
@@ -14,6 +14,24 @@ protocol NetworkManagerable {
 
 struct NetworkManager: NetworkManagerable {
     func request() {
+    private func urlRequest(_ requestable: Requestable) throws -> URLRequest {
+        guard let url = URL(string: requestable.baseURL + requestable.path) else {
+            throw APIError.urlError
+        }
         
+        var request = URLRequest(url: url)
+        request.httpMethod = requestable.httpMethod.rawValue
+        
+        requestable.header.forEach { key, value in
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        
+        do {
+            request.httpBody = try JSONSerialization.data(withJSONObject: requestable.params, options: [])
+        } catch {
+            throw APIError.httpBodyError
+        }
+        
+        return request
     }
 }
