dceb04059a9bf5acd2c7e402a9c0749c849bc235
doogie97 <chris_97@naver.com>
2022-12-13 01:01:54 +0900

    💎feat : 거래소 검색 카테고리 관련 모델 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/APIModels/MarketsAPI/MarketOptionsAPI.swift
@@ -0,0 +1,62 @@
+//
+//  MarketOptionsAPI.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/12/13.
+//
+
+import Foundation
+import Alamofire
+
+struct MarketOptionsAPI: Requestable {
+    let baseURL = Host.lostarkAPI.baseURL
+    let path = "/markets/options"
+    let header: [String : String] = [
+        "authorization" : "Bearer \(Bundle.main.lostarkAPIKey)"
+    ]
+    let params: [String : Any] = [:]
+    let httpMethod = HTTPMethod.get
+}
+
+struct MarketOptions: Decodable {
+    let categories: [Category]
+    
+    struct Category: Decodable {
+        let subs: [CategoryItem]
+        let code: Int?
+        let codeName: String?
+        
+        struct CategoryItem: Decodable {
+            let code: Int?
+            let codeName: String?
+        }
+    }
+    
+    let itemGrades: [String]
+    let itemTiers: [Int]
+    let classes: [String]
+}
+
+extension MarketOptions {
+    enum CodingKeys: String, CodingKey {
+        case categories = "Categories"
+        case itemGrades = "ItemGrades"
+        case itemTiers = "ItemTiers"
+        case classes = "Classes"
+    }
+}
+
+extension MarketOptions.Category {
+    enum CodingKeys: String, CodingKey {
+        case subs = "Subs"
+        case code = "Code"
+        case codeName = "CodeName"
+    }
+}
+
+extension MarketOptions.Category.CategoryItem {
+    enum CodingKeys: String, CodingKey {
+        case code = "Code"
+        case codeName = "CodeName"
+    }
+}
