fd45cf78fe25e70c89452866c2297de67a496115
doogie97 <chris_97@naver.com>
2022-12-20 19:36:09 +0900

    ⚰️del : 사용되지 않는 메서드 제거

--- LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
+++ LOARANG-MVVM/LOARANG-MVVM/LostarkAPI/NetworkManager.swift
@@ -10,7 +10,6 @@ import Alamofire
 
 protocol NetworkManagerable {
     func request<T: Decodable>(_ requestable: Requestable, resultType: T.Type) async throws -> T
-    func request<T: Decodable>(_ requestable: Requestable, resultType: T.Type, completion: @escaping (Result<T, APIError>) -> Void)
 }
 
 struct NetworkManager: NetworkManagerable {
@@ -22,33 +21,6 @@ struct NetworkManager: NetworkManagerable {
             return response
         case .failure(let error):
             throw error
-        }
-    }
-    
-    func request<T: Decodable>(_ requestable: Requestable, resultType: T.Type, completion: @escaping (Result<T, APIError>) -> Void) {
-        requestable.request.responseDecodable(of: T.self) { result in
-            guard result.error == nil else {
-                completion(.failure(APIError.transportError))
-                return
-            }
-
-            guard let response = result.response else {
-                completion(.failure(APIError.responseError))
-                return
-            }
-
-            guard (200...299).contains(response.statusCode) else {
-                print(response.statusCode)
-                completion(.failure(APIError.statusCodeError))
-                return
-            }
-
-            guard let value = result.value else {
-                completion(.failure(APIError.dataError))
-                return
-            }
-            
-            completion(.success(value))
         }
     }
 }
