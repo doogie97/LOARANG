52c9acdd3bf99e3f0ad0a310d8f8603ced35a0d9
doogie97 <chris_97@naver.com>
2022-12-29 01:27:28 +0900

    🖌️layout : category 설정 관련 view 생성

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/Market/MarketView.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/Market/MarketView.swift
@@ -17,7 +17,7 @@ final class MarketView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private(set) var itemSearchBar: UISearchBar = {
+    private(set) lazy var itemSearchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.searchBarStyle = .minimal
         searchBar.placeholder = "아이템 명"
@@ -25,13 +25,61 @@ final class MarketView: UIView {
         return searchBar
     }()
     
+    private lazy var categoryTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.one(size: 18, family: .Bold)
+        label.text = "카테고리"
+        label.textAlignment = .center
+        
+        return label
+    }()
+    
+    private(set) lazy var categoryButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("카테고리를 선택해 주세요", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = UIFont.one(size: 16, family: .Bold)
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.white.cgColor
+        button.layer.cornerRadius = 5
+        
+        return button
+    }()
+    
+    private lazy var directionImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .label
+        imageView.image = UIImage(systemName: "chevron.down")
+        
+        return imageView
+    }()
+    
     private func setLayout() {
         self.backgroundColor = .mainBackground
         
         self.addSubview(itemSearchBar)
+        self.addSubview(categoryTitle)
+        self.addSubview(categoryButton)
+        self.addSubview(directionImageView)
         
         itemSearchBar.snp.makeConstraints {
-            $0.top.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(15)
+            $0.top.leading.trailing.equalTo(self.safeAreaLayoutGuide).inset(16)
+        }
+        
+        categoryTitle.snp.makeConstraints {
+            $0.top.equalTo(itemSearchBar.snp.bottom).offset(16)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        categoryButton.snp.makeConstraints {
+            $0.height.equalTo(40)
+            $0.top.equalTo(categoryTitle.snp.bottom).offset(8)
+            $0.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        directionImageView.snp.makeConstraints {
+            $0.centerY.equalTo(categoryButton)
+            $0.trailing.equalTo(categoryButton.snp.trailing).inset(16)
         }
     }
 }
