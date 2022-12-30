afd04ddfdba559f259f25b470aba43a0d4a10604
doogie97 <chris_97@naver.com>
2022-12-30 15:07:29 +0900

    💎feat : SubOptionCell 생성

---
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/Market/SubOptionCell.swift
@@ -0,0 +1,39 @@
+//
+//  SubOptionCell.swift
+//  LOARANG-MVVM
+//
+//  Created by 최최성균 on 2022/12/30.
+//
+
+import SnapKit
+
+final class SubOptionCell: UITableViewCell {
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var optionTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.one(size: 18, family: .Bold)
+        
+        return label
+    }()
+    
+    private func setLayout() {
+        self.backgroundColor = .clear
+        self.contentView.addSubview(optionTitleLabel)
+        
+        optionTitleLabel.snp.makeConstraints {
+            $0.edges.equalToSuperview().inset(16)
+        }
+    }
+    
+    func setCellContents(optionTitle: String) {
+        optionTitleLabel.text = optionTitle
+    }
+}
