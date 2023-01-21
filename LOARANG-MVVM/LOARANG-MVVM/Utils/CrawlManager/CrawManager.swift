04c969be59811b38cc7af73b819ac3db5bee5067
doogie97 <chris_97@naver.com>
2023-01-21 13:39:02 +0900

    ♻️refactor : 크롤링시 async/await를 이용해 리턴값으로 제공하도록 수정

--- LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawManager.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Utils/CrawlManager/CrawManager.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftSoup
+import Alamofire
 
 protocol CrawlManagerable {
     func getUserInfo(_ name: String, completion: @escaping (Result<UserInfo, Error>) -> Void)
+    func getUserInfo2(_ name: String) async throws -> UserInfo
     func chenckInspection() throws
 }
 
@@ -60,6 +62,30 @@ struct CrawlManager: CrawlManagerable {
         }
     }
     
+    func getUserInfo2(_ name: String) async throws -> UserInfo {
+        guard let url = makeURL(urlString: baseURL, name: name) else {
+            throw CrawlError.urlError
+        }
+
+        let doc = try await makeDocument2(url: url)
+
+        guard let stat = try? getStat(doc: doc) else {
+            throw CrawlError.searchError
+        }
+
+        guard let userJsonInfo = try? getUserJsonInfo(doc: doc) else {
+            throw CrawlError.searchError
+        }
+
+        let userImage = await getUserImage2(name: name)
+        
+        guard let mainInfo = try? getMainInfo2(name: name, doc: doc, userImage: userImage) else {
+            throw CrawlError.searchError
+        }
+        
+        return UserInfo(mainInfo: mainInfo, stat: stat, userJsonInfo: userJsonInfo)
+    }
+    
     private func makeURL(urlString: String, name: String) -> URL? {
         guard let url = URL(string: urlString + name.changeToPercent()) else {
             return nil
@@ -70,6 +96,20 @@ struct CrawlManager: CrawlManagerable {
     
     private func makeDocument(url: URL) throws -> Document {
         guard let html = try? String(contentsOf: url, encoding: .utf8) else {
+            throw CrawlError.documentError
+        }
+        
+        guard let doc = try? SwiftSoup.parse(html) else {
+            throw CrawlError.documentError
+        }
+        
+        return doc
+    }
+    
+    private func makeDocument2(url: URL) async throws -> Document {
+        let response = await AF.request(url).serializingString().response
+
+        guard let html = response.value else {
             throw CrawlError.documentError
         }
         
@@ -106,6 +146,47 @@ struct CrawlManager: CrawlManagerable {
         } catch {
             completion(.failure(CrawlError.searchError))
         }
+    }
+    
+    private func getMainInfo2(name: String, doc: Document, userImage: UIImage) throws -> MainInfo {
+        
+        do {
+            let server = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__user > dl.myinfo__user-names > dd > div.wrapper-define > dl:nth-child(1) > dd").text()
+            
+            if server == "" {
+                throw CrawlError.searchError
+            }
+            
+            let name = try doc.select("#myinfo__character--button2").text().components(separatedBy: " ")[safe: 1] ?? "알 수 없음"
+            let battleLV = try doc.select("#myinfo__character--button2 > span").text().replacingOccurrences(of: "Lv.", with: "")
+            let itemLV = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(2) > dl.define.item > dd").text()
+            let expeditionLV = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(1) > dl:nth-child(1) > dd").text()
+            let title = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(1) > dl:nth-child(2) > dd").text()
+            let guild = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(3) > dl:nth-child(1) > dd").text()
+            let pvp = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(3) > dl:nth-child(2) > dd").text()
+            let wisdom = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__contents-level > div:nth-child(4) > dl > dd").text()
+            let `class` = try doc.select("#lostark-wrapper > div > main > div > div > div.myinfo__contents-character > div.myinfo__user > dl.myinfo__user-names > dd > div.wrapper-define > dl:nth-child(2) > dd").text()
+            
+            return MainInfo(server: server, name: name, battleLV: battleLV, itemLV: itemLV, expeditionLV: expeditionLV, title: title, guild: guild, pvp: pvp, wisdom: wisdom, class: `class`, userImage: userImage)
+        }
+    }
+    
+    private func getUserImage2(name: String) async -> UIImage {
+        let urlString = "https://lostark.game.onstove.com/Profile/Character/"
+        guard let url = makeURL(urlString: urlString, name: name),
+              let doc = try? makeDocument(url: url),
+              let imageURL = try? doc.select("#profile-equipment > div.profile-equipment__character > img").attr("src"),
+              let url = URL(string: imageURL) else {
+            return UIImage()
+        }
+        
+        let response = await AF.request(url).serializingData().response
+        
+        guard let data = response.data else {
+            return UIImage()
+        }
+        
+        return UIImage(data: data) ?? UIImage()
     }
     
     private func getUserImage(name: String, completion: @escaping (UIImage) -> Void) {
