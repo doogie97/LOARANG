e2df33a099a64370bce0bf42064a75d777bcbf67
Doogie <chris_97@naver.com>
2023-01-29 18:40:39 +0900

    💎feat : 키보드 관련 바인드 메서드 생성

--- LOARANG-MVVM/LOARANG-MVVM/Scenes/Market/MarketViewController.swift
+++ LOARANG-MVVM/LOARANG-MVVM/Scenes/Market/MarketViewController.swift
@@ -41,6 +41,7 @@ final class MarketViewController: UIViewController {
     private func bindView() {
         buttonTextBind()
         bindItemTableView()
+        bindKeyboard()
         marketView.marketOptionView.categoryButton.rx.tap
             .withUnretained(self)
             .bind { owner, _ in
@@ -234,6 +235,15 @@ final class MarketViewController: UIViewController {
                 if indexPath.last?.row == itemCount - 1 {
                     owner.viewModel.scrolledEndPoint()
                 }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func bindKeyboard() {
+        viewModel.hideKeyboard
+            .withUnretained(self)
+            .bind { (owner, _) in
+                owner.view.endEditing(true)
             }
             .disposed(by: disposeBag)
     }
