a57136999d703ec4c01c883466e378172335f75a
Doogie <chris_97@naver.com>
2024-04-11 17:42:21 +0900

    feat: 자주 사용하는 폰트 UIView 확장 메서드로 구현

--- LOARANG/LOARANG/Utils/Extesnsion/UIView + Extension.swift
+++ LOARANG/LOARANG/Utils/Extesnsion/UIView + Extension.swift
@@ -30,6 +30,24 @@ extension UIView {
         return UIScreen.main.bounds.height <= 667
     }
     
+    func oneFontLabel(size: Int = 14, family: UIFont.Family = .Bold, color: UIColor = .white, text: String = "") -> UILabel {
+        let label = UILabel()
+        label.font = UIFont.one(size: size, family: family)
+        label.textColor = color
+        label.text = text
+        
+        return label
+    }
+    
+    func blackHanSansLabel(size: Int = 20, color: UIColor = .white, text: String = "") -> UILabel {
+        let label = UILabel()
+        label.font = UIFont.BlackHanSans(size: size)
+        label.textColor = color
+        label.text = text
+        
+        return label
+    }
+    
     var adMobView: GADBannerView {
         let bannerView = GADBannerView()
 #if DEBUG
