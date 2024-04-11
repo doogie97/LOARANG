d8f9bc0f4acaa5ea87f4a941abb7e33665b661c7
Doogie <chris_97@naver.com>
2024-04-11 16:57:30 +0900

    feat: HomeSectionCV 레이아웃 구성
    임시 구성

--- LOARANG/LOARANG/Presentaion/Home/HomeView/HomeSectionView/HomeSectionView.swift
+++ LOARANG/LOARANG/Presentaion/Home/HomeView/HomeSectionView/HomeSectionView.swift
@@ -9,8 +9,15 @@ import UIKit
 import SnapKit
 
 final class HomeSectionView: UIView {
+    enum SectionCase: Int, CaseIterable {
+        case mainUser
+        case bookmark
+        case event
+        case notice
+    }
+    
     private lazy var sectionCV = UICollectionView(frame: .zero,
-                                                              collectionViewLayout: UICollectionViewLayout())
+                                                  collectionViewLayout: UICollectionViewLayout())
     
     func setViewContents() {
         setLayout()
@@ -21,5 +28,110 @@ final class HomeSectionView: UIView {
         sectionCV.snp.makeConstraints {
             $0.edges.equalToSuperview()
         }
+    }
+}
+
+extension HomeSectionView: UICollectionViewDelegate, UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return SectionCase.allCases.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let section = SectionCase(rawValue: section) else {
+            return 0
+        }
+        
+        switch section {
+        case .mainUser:
+            return 1
+        case .bookmark:
+            return 10 //viewModel의 bookmarkCount만큼
+        case .event:
+            return 6 //viewModel의 event수 만큼
+        case .notice:
+            return 8 //viewModel의 notice수 만큼 인데 최대 개수 몇 개로 할 지 고민 필요
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell() //cell 생성 전 임시 return
+    }
+}
+
+//MARK: - Make CollectionView Layout
+extension HomeSectionView {
+    private func createSectionCV() -> UICollectionView {
+        let layout = createSectionCVLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        return collectionView
+    }
+    
+    private func createSectionCVLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { [weak self] (sectionIndex, _ ) -> NSCollectionLayoutSection? in
+            guard let sectionCase = SectionCase(rawValue: sectionIndex) else {
+                return nil
+            }
+            
+            switch sectionCase {
+            case .mainUser:
+                return self?.mainUserSectionLayout()
+            case .bookmark:
+                return self?.bookmarkSectionLayout()
+            case .event:
+                return self?.eventSectionLayout()
+            case .notice:
+                return self?.noticeSectionLayout()
+            }
+        }
+    }
+    
+    func mainUserSectionLayout() -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        return section
+    }
+    
+    func bookmarkSectionLayout() -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        return section
+    }
+    
+    func eventSectionLayout() -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        return section
+    }
+    
+    func noticeSectionLayout() -> NSCollectionLayoutSection {
+        let size = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let item = NSCollectionLayoutItem(layoutSize: size)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(315 / 393))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 0, leading: 0, bottom: 10, trailing: 0)
+        
+        return section
     }
 }
