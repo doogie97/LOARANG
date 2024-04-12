4525ccc141caa7dfaf73d4337914ba383d26161e
Doogie <chris_97@naver.com>
2024-04-12 12:43:55 +0900

    feat: ImageCell에 title View 구현

--- LOARANG/LOARANG/Presentaion/Home/HomeView/HomeSectionView/HomeSectionCVCell/HomeImageCVCell.swift
+++ LOARANG/LOARANG/Presentaion/Home/HomeView/HomeSectionView/HomeSectionCVCell/HomeImageCVCell.swift
@@ -19,15 +19,47 @@ final class HomeImageCVCell: UICollectionViewCell {
         return imageView
     }()
     
+    private lazy var imageTitleLabel = pretendardLabel(size: 16, family: .Bold, color: #colorLiteral(red: 0.919945538, green: 0.8131091595, blue: 0.5661830306, alpha: 1))
+    private lazy var titleLabelView = {
+        let view = UIView()
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        view.layer.cornerRadius = 6
+        view.clipsToBounds = true
+        let backView = UIView()
+        backView.backgroundColor = .black
+        backView.layer.opacity = 0.7
+        view.addSubview(backView)
+        view.addSubview(imageTitleLabel)
+        
+        backView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        imageTitleLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(margin(.width, 10))
+            $0.top.trailing.bottom.equalToSuperview()
+        }
+        
+        return view
+    }()
+    
     func setCellContents(imageUrl: String, imageTitle: String?) {
         self.imageViewSessionTask = imageView.setImage(urlString: imageUrl)
+        imageTitleLabel.text = imageTitle
+        self.titleLabelView.isHidden = imageTitle == nil
         setLayout()
     }
     
     private func setLayout() {
         self.contentView.addSubview(imageView)
+        self.contentView.addSubview(titleLabelView)
         imageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+        
+        titleLabelView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.height.equalTo(35)
         }
     }
     
