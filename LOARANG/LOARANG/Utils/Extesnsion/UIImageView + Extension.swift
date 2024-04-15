e9d92e42b19eeaabeb7fd54b5d0b97ca422e776d
Doogie <chris_97@naver.com>
2024-04-15 10:54:08 +0900

    refactor: 이미지 할당시 completion 클로저 생성

--- LOARANG/LOARANG/Utils/Extesnsion/UIImageView + Extension.swift
+++ LOARANG/LOARANG/Utils/Extesnsion/UIImageView + Extension.swift
@@ -9,13 +9,22 @@ import UIKit
 import Kingfisher
 
 extension UIImageView {
-    func setImage(_ imageUrl: String?) {
+    func setImage(_ imageUrl: String?, completion: ((UIImage) -> Void)? = nil) {
         guard let url = URL(string: imageUrl ?? "") else {
             return
         }
         let options: KingfisherOptionsInfo = [.cacheMemoryOnly]
         self.kf.setImage(with: url,
-                         options: options)
+                         options: options) { result in
+            switch result {
+            case .success(let value):
+                if let completion = completion {
+                    completion(value.image)
+                }
+            case .failure(_):
+                break
+            }
+        }
     }
     
     func setImage(urlString: String?) -> URLSessionDataTask? {
