c17a0bfd525ffbf75e98b45d92e3d30f1b7d2084
Doogie <chris_97@naver.com>
2024-04-18 10:47:41 +0900

    feat: 보유 캐릭터 cell 임시 생성

---
+++ LOARANG/LOARANG/Presentaion/CharacterDetail/CharacterDetailView/CharacterDetailPageVC/CharacterDetailOwnCharacterCell.swift
@@ -0,0 +1,35 @@
+//
+//  CharacterDetailOwnCharacterCell.swift
+//  LOARANG
+//
+//  Created by Doogie on 4/18/24.
+//
+
+import UIKit
+import SnapKit
+import Kingfisher
+
+final class CharacterDetailOwnCharacterCell: UITableViewCell {
+    private lazy var classImageView = UIImageView()
+    
+    func setCellContents(_ character: OwnCharactersEntity.Character) {
+        classImageView.setImage(character.characterClass.classImageURL)
+        setLayout()
+    }
+    
+    private func setLayout() {
+        self.contentView.addSubview(classImageView)
+        classImageView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(24)
+            $0.leading.equalToSuperview().inset(18)
+            $0.height.equalTo(40)
+            $0.width.equalTo(classImageView.snp.height)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        classImageView.kf.cancelDownloadTask()
+        classImageView.image = nil
+    }
+}
