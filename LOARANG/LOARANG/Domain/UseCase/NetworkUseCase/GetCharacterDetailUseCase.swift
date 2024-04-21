4d34d23700b60129c1a327d5d6e4294b8b5b61ba
Doogie <chris_97@naver.com>
2024-04-21 10:31:39 +0900

    refactor: 가독성을 위한 코드 분리

--- LOARANG/LOARANG/Domain/UseCase/NetworkUseCase/GetCharacterDetailUseCase.swift
+++ LOARANG/LOARANG/Domain/UseCase/NetworkUseCase/GetCharacterDetailUseCase.swift
@@ -65,6 +65,7 @@ struct GetCharacterDetailUseCase {
     private func equipments(_ dto: [CharactersDetailDTO.Equipment]?) -> [CharacterDetailEntity.Equipment] {
         return (dto ?? []).compactMap {
             let equipmentDetailInfo = equipmentDetailInfo($0.Tooltip)
+            print(equipmentDetailInfo.engraving)
             return CharacterDetailEntity.Equipment(
                 equipmentType: EquipmentType(rawValue: $0.equipmentType ?? "") ?? .unknown,
                 name: $0.Name ?? "",
@@ -86,14 +87,6 @@ struct GetCharacterDetailUseCase {
     private func equipmentDetailInfo(_ tooltip: String?) -> EquipmentDetailInfo {
         let jsonData = JSON((tooltip ?? "").data(using: .utf8) ?? Data())
         
-        //상급재련
-        var highReforgingLevel: Int?
-        let highReforgingInfo = jsonData["Element_005"]["value"].stringValue
-        if highReforgingInfo.contains("상급 재련") {
-            let str = highReforgingInfo.components(separatedBy: ">").joined().components(separatedBy: "'")[safe: 6]?.components(separatedBy: "<").first ?? ""
-            highReforgingLevel = Int(str)
-        }
-        
         var basicEffect = [String]()
         var additionalEffect = [String]()
         var setOption = ""
@@ -105,64 +98,30 @@ struct GetCharacterDetailUseCase {
             let firstParseJson = jsonData["Element_\(number.formattedNumber)"]["value"]
             
             let firstParseJson000Str = firstParseJson["Element_000"].stringValue
-            //기본 효과
+            
             if firstParseJson000Str.contains("기본 효과") {
                 basicEffect = firstParseJson["Element_001"].stringValue.components(separatedBy: "<BR>").compactMap { return $0.insideAngleBrackets }
             }
             
-            // 추가 효과
             if firstParseJson000Str.contains("추가 효과") || firstParseJson000Str.contains("세공 단계") {
                 additionalEffect = firstParseJson["Element_001"].stringValue.components(separatedBy: "<BR>").compactMap { return $0.insideAngleBrackets }
             }
             
-            //세트 옵션
+            
             if firstParseJson000Str.contains("세트 효과 레벨") {
                 setOption = firstParseJson["Element_001"].stringValue
             }
             
-            //각인 효과
+            
             if firstParseJson["Element_000"]["topStr"].stringValue.contains("각인 효과") {
-                let engravingsJson = firstParseJson["Element_000"]["contentStr"]
-                var elementIndex = 0
-                while elementIndex != -1 {
-                    if elementIndex != -1 {
-                        let engravingStr = engravingsJson["Element_\(elementIndex.formattedNumber)"]["contentStr"].stringValue
-                        if engravingStr.contains("활성도"){
-                            let separated = engravingStr.components(separatedBy: "] ")
-                            let name = separated.first?.insideAngleBrackets ?? ""
-                            let value = Int(separated.last?.replacingOccurrences(of: "<BR>", with: "").replacingOccurrences(of: "활성도 ", with: "") ?? "") ?? 0
-                            engraving.append((name: name, value: value))
-                            elementIndex += 1
-                        } else {
-                            elementIndex = -1
-                        }
-                    }
-                }
+                engraving = parseEngraving(firstParseJson)
             }
             
             let secondParseJsonStr = firstParseJson["Element_000"]["topStr"].stringValue
-            //엘릭서
             if secondParseJsonStr.contains("엘릭서") {
-                elixirs = [CharacterDetailEntity.Elixir]()
-                let elixirsJson = firstParseJson["Element_000"]["contentStr"]
-                var elementIndex = 0
-                while elementIndex != -1 {
-                    let elixirString = elixirsJson["Element_\(elementIndex.formattedNumber)"]["contentStr"].stringValue
-                    if elixirString.isEmptyString {
-                        elementIndex = -1
-                    } else {
-                        let searatedStrArr = elixirString.components(separatedBy: ">")
-                        elixirs?.append(CharacterDetailEntity.Elixir(
-                            name: searatedStrArr[safe: 2]?.components(separatedBy: " <").first ?? "",
-                            level: searatedStrArr[safe: 3]?.replacingOccurrences(of: "</FONT", with: "") ?? "",
-                            effect: searatedStrArr.last ?? "")
-                        )
-                        elementIndex += 1
-                    }
-                }
+                elixirs = parseElixirs(firstParseJson)
             }
             
-            //초월
             if secondParseJsonStr.contains("[초월]") {
                 let separatedStrArr = secondParseJsonStr.components(separatedBy: ">")
                 transcendence = CharacterDetailEntity.Transcendence(
@@ -172,15 +131,14 @@ struct GetCharacterDetailUseCase {
             }
         }
         
-        let reinforcementLevel = Int(jsonData["Element_000"]["value"].stringValue.components(separatedBy: ">+").last?.components(separatedBy: " ").first ?? "") ?? 0
         let itemTitle = jsonData["Element_001"]["value"]
         
         return EquipmentDetailInfo(
             qualityValue: itemTitle["qualityValue"].intValue,
             itemLevel: Int(itemTitle["leftStr2"].stringValue.components(separatedBy: " ")[safe: 3] ?? "") ?? 0,
             itemTypeTitle: itemTitle["leftStr0"].stringValue.insideAngleBrackets,
-            reinforcementLevel: reinforcementLevel,
-            highReforgingLevel: highReforgingLevel,
+            reinforcementLevel: Int(jsonData["Element_000"]["value"].stringValue.components(separatedBy: ">+").last?.components(separatedBy: " ").first ?? "") ?? 0,
+            highReforgingLevel: parseHighReforgingLevel(jsonData),
             basicEffect: basicEffect,
             additionalEffect: additionalEffect,
             setOptionName: setOption.components(separatedBy: " <").first ?? "",
@@ -189,6 +147,59 @@ struct GetCharacterDetailUseCase {
             transcendence: transcendence, 
             engraving: engraving
         )
+    }
+    
+    private func parseHighReforgingLevel(_ jsonData: JSON) -> Int? {
+        let highReforgingInfo = jsonData["Element_005"]["value"].stringValue
+        if highReforgingInfo.contains("상급 재련") {
+            let str = highReforgingInfo.components(separatedBy: ">").joined().components(separatedBy: "'")[safe: 6]?.components(separatedBy: "<").first ?? ""
+            return Int(str)
+        } else {
+            return nil
+        }
+    }
+    
+    private func parseEngraving(_ firstParseJson: JSON) -> [(name: String, value: Int)] {
+            var engraving = [(name: String, value: Int)]()
+            let engravingsJson = firstParseJson["Element_000"]["contentStr"]
+            var elementIndex = 0
+            while elementIndex != -1 {
+                if elementIndex != -1 {
+                    let engravingStr = engravingsJson["Element_\(elementIndex.formattedNumber)"]["contentStr"].stringValue
+                    if engravingStr.contains("활성도"){
+                        let separated = engravingStr.components(separatedBy: "] ")
+                        let name = separated.first?.insideAngleBrackets ?? ""
+                        let value = Int(separated.last?.replacingOccurrences(of: "<BR>", with: "").replacingOccurrences(of: "활성도 ", with: "") ?? "") ?? 0
+                        engraving.append((name: name, value: value))
+                        elementIndex += 1
+                    } else {
+                        elementIndex = -1
+                    }
+                }
+            }
+            return engraving
+    }
+    
+    private func parseElixirs(_ firstParseJson: JSON) -> [CharacterDetailEntity.Elixir] {
+        var elixirs = [CharacterDetailEntity.Elixir]()
+        
+        let elixirsJson = firstParseJson["Element_000"]["contentStr"]
+        var elementIndex = 0
+        while elementIndex != -1 {
+            let elixirString = elixirsJson["Element_\(elementIndex.formattedNumber)"]["contentStr"].stringValue
+            if elixirString.isEmptyString {
+                elementIndex = -1
+            } else {
+                let searatedStrArr = elixirString.components(separatedBy: ">")
+                elixirs.append(CharacterDetailEntity.Elixir(
+                    name: searatedStrArr[safe: 2]?.components(separatedBy: " <").first ?? "",
+                    level: searatedStrArr[safe: 3]?.replacingOccurrences(of: "</FONT", with: "") ?? "",
+                    effect: searatedStrArr.last ?? "")
+                )
+                elementIndex += 1
+            }
+        }
+        return elixirs
     }
     
     struct EquipmentDetailInfo {
