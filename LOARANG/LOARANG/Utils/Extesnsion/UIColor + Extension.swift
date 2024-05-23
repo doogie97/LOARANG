4466d5158ffdd79a10220f1d9d1114196b934f71
Doogie <chris_97@naver.com>
2024-05-23 13:35:23 +0900

    refactor: color 기본값 설정

--- LOARANG/LOARANG/Utils/Extesnsion/UIColor + Extension.swift
+++ LOARANG/LOARANG/Utils/Extesnsion/UIColor + Extension.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 extension UIColor {
-    static let mainBackground = UIColor(named: "mainBackground")
-    static let buttonColor = UIColor(named: "buttonColor")
-    static let tableViewColor = UIColor(named: "tableViewColor")
-    static let cellBackgroundColor = UIColor(named: "cellBackgroundColor")
-    static let cellColor = UIColor(named: "cellColor")
+    static let mainBackground = UIColor(named: "mainBackground") ?? .clear
+    static let buttonColor = UIColor(named: "buttonColor") ?? .clear
+    static let tableViewColor = UIColor(named: "tableViewColor") ?? .clear
+    static let cellBackgroundColor = UIColor(named: "cellBackgroundColor") ?? .clear
+    static let cellColor = UIColor(named: "cellColor") ?? .clear
 }
